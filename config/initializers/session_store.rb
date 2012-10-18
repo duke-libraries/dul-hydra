6bf3b68eb934493ffa4c362c876c7de1a627529c
Jim Coble <jim.coble@duke.edu>
2012-10-18 10:34:20 -0400

    Skeleton Rails app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DulHydra::Application.config.session_store :cookie_store, key: '_dul-hydra_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DulHydra::Application.config.session_store :active_record_store
