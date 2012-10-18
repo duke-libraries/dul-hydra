f82abd7d8ff7cdc04c31aefbfa946e45eccb4248
dchandekstark <dchandekstark@gmail.com>
2012-10-18 17:08:31 -0400

    Resolved Issue #1

--- spec/models/component_spec.rb
+++ spec/models/component_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe Component do
+
   before do
     @pid = "test:1"
+    c = Component.new(:pid => @pid)
+    c.save!
   end
   
-  it "should retrieve a component" do
+  it "should retrieve the component" do
     c = Component.find(@pid)
-    c.pid.should == @pid
+    c.pid.should eq(@pid)
+    c.datastreams.should have_key("DC")
+    c.datastreams.should have_key("RELS-EXT")
   end
+  
 end
