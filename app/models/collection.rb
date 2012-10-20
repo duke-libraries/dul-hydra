35ff468e563e914d806b6faabf146aa95b4c554e
dchandekstark <dchandekstark@gmail.com>
2012-10-20 11:52:11 -0400

    Implemented isMemberOf relationship b/w Item and Collection - closes #10

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,7 @@
 class Collection < ActiveFedora::Base
+  
+  include ActiveFedora::Relationships
+
+  has_relationship "members", :is_member_of, :inbound => true
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ActiveFedora::Base
+
+  include ActiveFedora::Relationships
+
+  has_relationship "member_of", :is_member_of
+  
 end
