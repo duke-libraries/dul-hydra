0c6f733a423a6a5306065d5aa84e103953a960e7
dchandekstark <dchandekstark@gmail.com>
2012-10-20 11:54:23 -0400

    Implemented test for issue #10

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,12 +3,20 @@ require 'spec_helper'
 describe Item do
 
   before do
-    @pid = "item:1"
-    @item = Item.create(:pid => @pid)
+    @item_pid = "item:1"
+    @item = Item.create(:pid => @item_pid)
+    @collection_pid = "collection:1"
+    @collection = Collection.create(:pid => @collection_pid)
   end
 
   after do
     @item.delete
+  end
+
+  it "should be able to become a member of a collection" do
+    @item.member_of_append(@collection)
+    @item.save
+    @item.member_of.should include(@collection)
   end
 
 end
