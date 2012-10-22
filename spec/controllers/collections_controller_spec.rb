f0de459c3db840dc8276a1a397c05eb6b82190ff
dchandekstark <dchandekstark@gmail.com>
2012-10-22 11:38:15 -0400

    reverted 2 files to commit d44011b6e71a95ca679169704a5415c01da789d0. closes #8

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -1,60 +1,43 @@
 require 'spec_helper'
 
-RSpec.configure do |c|
-  # declare an exclusion filter
-  c.filter_run_excluding :broken => true
-end
-
 describe CollectionsController do
 
   describe "#index" do
-
     before do
-      @collection1 = Collection.create(:pid => "collection:1")
-      @collection2 = Collection.create(:pid => "collection:2")
+      @collection1 = Collection.create(:pid => "test:1")
+      @collection2 = Collection.create(:pid => "test:2")
     end
-
+    it "should display a list of all the collections" do
+      get :index
+      response.should be_successful
+      assigns[:collections].should == [@collection1, @collection2]
+    end
     after do
       @collection1.delete
       @collection2.delete
     end
-
-    it "should display a list of all the collections" do
-      get :index
-      response.should be_successful
-      assigns[:collections].should include(@collection1)
-      assigns[:collections].should include(@collection2)
-    end
-
-  end #index
+  end
   
   describe "#new" do
-
     it "should set a template collection" do
       get :new
       response.should be_successful
       assigns[:collection].should be_kind_of Collection
     end
-
-  end #new
+  end
   
   describe "#create" do
-
+    before do
+      @count = Collection.count
+      @pid = "test:1"
+    end
+    it "should create a collection" do
+      post :create, :collection=>{:pid=>@pid}
+      response.should redirect_to collections_path
+      Collection.count.should == @count + 1
+    end
     after do
       Collection.find(@pid).delete
     end
-
-    before do
-      @count = Collection.count
-      @pid = "collection:1"
-    end
-
-    it "should create a collection", :broken => true do
-      post :create, :collection => {:pid => @pid}
-      response.should redirect_to collections_path
-      Collection.count.should eq(@count + 1)
-    end
-
-  end #create
-
+  end
 end
--- spec/requests/collections_spec.rb
+++ spec/requests/collections_spec.rb
@@ -1,49 +1,26 @@
 require 'spec_helper'
 
-RSpec.configure do |c|
-  # declare an exclusion filter
-  c.filter_run_excluding :broken => true
-end
-
 describe "Collections" do
-
-  @pid1 = "collection:1"
-  @pid2 = "collection:2"
-
   describe "GET /collections" do
-
     before do
-      @collection1 = Collection.create(:pid => @pid1)
-      @collection2 = Collection.create(:pid => @pid2)
+      @collection1 = Collection.create(:pid => "test:1")
+      @collection2 = Collection.create(:pid => "test:2")
     end
-
     it "should display a list of all collections" do
       visit collections_path
-      page.should have_content @pid1
-      page.should have_content @pid2
+      page.should have_content "test:1"
+      page.should have_content "test:2"
     end
-
     after do
       @collection1.delete
       @collection2.delete
     end
-
-  end # GET (index/list)
-
-  describe "POST /collections" do
-
-    it "should create a collection", :broken => true do
-      visit new_collection_path
-      fill_in "Pid", :with => @pid1
-      click_button "Create Collection"
-      page.should have_content "Added Collection"
-      page.should have_content @pid1
-    end
-
-    after do
-      Collection.find(@pid1).delete
-    end
-
-  end # POST (create)
-
+  end
+  it "should create a collection" do
+    visit new_collection_path
+    fill_in "Pid", :with => "test:1"
+    click_button "Create"
+    page.should have_content "Added Collection"
+    page.should have_content "test:1"
+  end
 end
