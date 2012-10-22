0ecad1d9ad50dcbb5563c6574c182bd0b6a2dd0c
dchandekstark <dchandekstark@gmail.com>
2012-10-22 17:55:34 -0400

    closes #3

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -11,4 +11,18 @@ describe Collection do
     @collection.delete
   end
 
+  it "should have a DC datastream" do
+    @collection.datastreams["DC"].should_not be_nil
+  end
+
+  it "should have a RELS-EXT datastream" do
+    @collection.datastreams["RELS-EXT"].should_not be_nil
+  end
+
+  it "should be able to retrieve the item" do
+    c = Collection.find(@collection_pid)
+    c.pid.should eq(@collection_pid)
+  end
+
+
 end
