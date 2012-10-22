7b08604a084b9fb8200b4f00383b5baf7a8ec5fa
Jim Coble <jim.coble@duke.edu>
2012-10-22 16:44:02 -0400

    refactored to change pid's used and to eliminate certain string literals

--- spec/requests/collections_spec.rb
+++ spec/requests/collections_spec.rb
@@ -3,24 +3,34 @@ require 'spec_helper'
 describe "Collections" do
   describe "GET /collections" do
     before do
-      @collection1 = Collection.create(:pid => "test:1")
-      @collection2 = Collection.create(:pid => "test:2")
+      @pid1 = "collection:1"
+      @pid2 = "collection:2"
+      @collection1 = Collection.create(:pid => @pid1)
+      @collection2 = Collection.create(:pid => @pid2)
     end
     it "should display a list of all collections" do
       visit collections_path
-      page.should have_content "test:1"
-      page.should have_content "test:2"
+      page.should have_content @pid1
+      page.should have_content @pid2
     end
     after do
       @collection1.delete
       @collection2.delete
     end
   end
-  it "should create a collection" do
-    visit new_collection_path
-    fill_in "Pid", :with => "test:1"
-    click_button "Create"
-    page.should have_content "Added Collection"
-    page.should have_content "test:1"
+  describe "POST /collection" do
+    before do
+      @pid = "collection:1"
+    end
+    after do
+      Collection.find(@pid).delete
+    end
+    it "should create a collection" do
+      visit new_collection_path
+      fill_in "Pid", :with => @pid
+      click_button "Create Collection"
+      page.should have_content "Added Collection"
+      page.should have_content @pid
+    end
   end
 end
