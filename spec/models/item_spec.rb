265aefb7a257f0c348e11d03469cc479f83378bd
dchandekstark <dchandekstark@gmail.com>
2012-10-22 17:53:02 -0400

    closes #2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,6 +14,19 @@ describe Item do
     @collection.delete
   end
 
+  it "should have a DC datastream" do
+    @item.datastreams["DC"].should_not be_nil
+  end
+
+  it "should have a RELS-EXT datastream" do
+    @item.datastreams["RELS-EXT"].should_not be_nil
+  end
+
+  it "should be able to retrieve the item" do
+    i = Item.find(@item_pid)
+    i.pid.should eq(@item_pid)
+  end
+
   it "should be able to become a member of a collection" do
     @item.member_of_append(@collection)
     @item.save
