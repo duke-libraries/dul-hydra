24c3368d664250370e4355beac5fd6120e2c9aec
dchandekstark <david.chandek.stark@duke.edu>
2012-11-13 17:43:33 -0500

    Closes #39

--- spec/requests/catalog_spec.rb
+++ spec/requests/catalog_spec.rb
@@ -11,9 +11,17 @@ describe "Catalog" do
   end
   it "should find a component by identifier" do # issue 34
     visit catalog_index_path
-    fill_in "q", :with => @component_identifier
     select "Identifier", :from => "search_field"
+    fill_in "q", :with => @component_identifier
     click_button "search"
     page.should have_content(@component_title)
+  end
+  it "should display PID, identifier, and model for each item in results" do
+    visit catalog_index_path
+    fill_in "q", :with => @component_title
+    click_button "search"
+    page.should have_content(@component.pid)
+    page.should have_content(@component.identifier.first)
+    page.should have_content("Component")
   end
 end
