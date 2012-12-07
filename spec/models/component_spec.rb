e678e485b8c560fe171890c3eb8b001b0af0e022
dchandekstark <dchandekstark@gmail.com>
2012-12-06 23:31:55 -0500

    refactored to use shared examples

--- spec/models/component_spec.rb
+++ spec/models/component_spec.rb
@@ -1,55 +1,29 @@
 require 'spec_helper'
+require 'shared_examples_for_describables'
 
 describe Component do
-  before do
-    @identifier = "test010010010"
-    @title = "Awesome photo"
-    @component = Component.create(:identifier => @identifier, :title => @title)
-  end
-  after do
-    @component.delete
-  end
-  it "should have the right datastreams" do
-    @component.datastreams["DC"].should be_kind_of ActiveFedora::Datastream
-    @component.datastreams["RELS-EXT"].should be_kind_of ActiveFedora::RelsExtDatastream
-    @component.datastreams["rightsMetadata"].should be_kind_of Hydra::Datastream::RightsMetadata
-  end
-  it "should be able to have an identifier" do # issue 27
-    pending "move test to describable_spec"
-    @component.identifier.first.should eq(@identifier)
-  end
-  it "should be findable by identifier" do
-    pending "move test to describable_spec"
-    results = Component.find_by_identifier(@identifier)
-    results.should include(@component)
-  end
-  it "should be findable by a truncated identifier" do # issue 33
-    pending "indexing identifier as symbol"
-    trunc_id = @identifier[0, @identifier.length - 1]
-    results = Component.find_by_identifier(trunc_id)
-    results.should include(@component)
-  end
-  it "should be able to have a title" do # issue 28
-    pending "move test to describable_spec"
-    @component.title.first.should eq(@title)
-  end
-  describe "relationships" do
+  it_behaves_like "a describable"
+  context "relationships" do
     before do
+      @component = Component.create
       @item = Item.create
     end
     after do
       @item.delete
+      @component.delete
     end
-    it "should be able to add itself to an item's list of components" do
-      @component.container = @item
-      @component.save
-      @component.container.should eq(@item)
-      @item.parts.should include(@component)
+    context "when container set to item" do
+      it "should be a part of the item" do
+        @component.container = @item
+        @component.save
+        @item.parts.should include(@component)
+      end
     end
-    it "should be able to be added by an item to its list of components" do
-      @item.parts << @component
-      @item.parts.should include(@component)
-      @component.container.should eq(@item)
+    context "when added to item's parts" do
+      it "should have the item as container" do
+        @item.parts << @component
+        @component.container.should eq(@item)
+      end
     end
   end # relationships
 end
