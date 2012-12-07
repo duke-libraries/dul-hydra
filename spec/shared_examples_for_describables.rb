32665da5938c9d3e5ff324d69cc4c2d3db53aed5
dchandekstark <dchandekstark@gmail.com>
2012-12-06 23:31:32 -0500

    new module

---
+++ spec/shared_examples_for_describables.rb
@@ -0,0 +1,35 @@
+shared_examples "a describable" do
+  before do
+    @identifier = "test010010010"
+    @title = "Describable Object"
+    @describable = described_class.create
+  end
+  after do
+    @describable.delete
+  end
+  context "#display_title" do
+    context "not having a title" do
+      it "should return the pid" do
+        @describable.display_title.should eq(@describable.pid)
+      end
+    end
+    context "having a title" do
+      before do
+        @describable.title = @title
+        @describable.save
+      end
+      it "should return the title" do
+        @describable.display_title.should eq(@describable.title.first)
+      end
+    end
+  end
+  context "having an identifier" do
+    before do
+      @describable.identifier = @identifier
+      @describable.save
+    end
+    it "should be findable by identifier" do
+      described_class.find_by_identifier(@identifier).should include(@describable)
+    end
+  end
+end
