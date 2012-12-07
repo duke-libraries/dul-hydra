e9c27e50e3d73de5b4702c06d440b9c95a4b4805
dchandekstark <dchandekstark@gmail.com>
2012-12-06 23:36:56 -0500

    refactored to use shared examples

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,18 +1,6 @@
 require 'spec_helper'
+require 'shared_examples_for_describables'
 
 describe Collection do
-  before do
-    @collection = Collection.create
-  end
-  after do
-    @collection.delete
-  end
-  it "should have the right datastreams" do
-    @collection.datastreams["DC"].should be_kind_of ActiveFedora::Datastream
-    @collection.datastreams["RELS-EXT"].should be_kind_of ActiveFedora::RelsExtDatastream
-    @collection.datastreams["rightsMetadata"].should be_kind_of Hydra::Datastream::RightsMetadata
-  end
-  # it "should have default permissions" do
-  #   @collection.permissions.should_not be_empty
-  # end
+  it_behaves_like "a describable"
 end
