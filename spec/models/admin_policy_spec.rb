57ebc2c799a3013be903854e869087a504e939b2
dchandekstark <dchandekstark@gmail.com>
2012-12-06 23:38:34 -0500

    trivial change

--- spec/models/admin_policy_spec.rb
+++ spec/models/admin_policy_spec.rb
@@ -4,15 +4,7 @@ describe AdminPolicy do
   it "should raise an exception when calling default and default APO has not been created" do
     lambda { AdminPolicy.get_default_apo }.should raise_error(ActiveFedora::ObjectNotFoundError)
   end
-  # describe "default!" do
-  #   after do
-  #     AdminPolicy.default.delete
-  #   end
-  #   it "should have the default APO" do
-  #     AdminPolicy.default!.should be_kind_of(AdminPolicy)
-  #   end
-  # end
-  describe "create_default_apo!" do
+  context "#create_default_apo!" do
     before do
       AdminPolicy.create_default_apo!
     end
