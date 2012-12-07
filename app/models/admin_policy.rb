7dd5bc649cd631587e44408111bad70dd9d75b2d
dchandekstark <dchandekstark@gmail.com>
2012-12-06 21:35:21 -0500

    removed deleted Permissible

--- app/models/admin_policy.rb
+++ app/models/admin_policy.rb
@@ -1,20 +1,18 @@
-# AdminPolicy does not subclass DulHydra::Models::Base
-# b/c Hydra::AdminPolicy provides all the datastreams it needs.
 class AdminPolicy < Hydra::AdminPolicy
-
-  include DulHydra::Models::Permissible
+  # AdminPolicy does not subclass DulHydra::Models::Base
+  # b/c Hydra::AdminPolicy provides all the datastreams it needs.
 
   APO_NAMESPACE = "duke-apo"
   # Fedora PID of the default APO object
   DEFAULT_APO_PID = "#{APO_NAMESPACE}:default"
                                    
   # Standard APO permission (by convention)
-  APO_PERMISSIONS = [ADMIN_GROUP_ACCESS]
+  APO_PERMISSIONS = [DulHydra::Permissions::ADMIN_GROUP_ACCESS]
 
   # Create the default APO and return it
   def self.create_default_apo!
     apo = AdminPolicy.new(:pid => DEFAULT_APO_PID, :label => "Default Admin Policy")
-    apo.default_permissions = DEFAULT_PERMISSIONS # defined in Permissible
+    apo.default_permissions = DulHydra::Permissions::DEFAULT_PERMISSIONS
     apo.permissions = APO_PERMISSIONS
     apo.save!
     return apo
