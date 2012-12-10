c584a62e7956d1093557b6f85628afa780e39ef6
dchandekstark <dchandekstark@gmail.com>
2012-12-10 18:00:31 -0500

    Added REGISTERED_DISCOVER_ACCESS

--- lib/dul_hydra/permissions.rb
+++ lib/dul_hydra/permissions.rb
@@ -6,6 +6,7 @@ module DulHydra::Permissions
     PUBLIC_READ_ACCESS = {:name => "public", :type => "group", :access => "read"}
     PUBLIC_DISCOVER_ACCESS = {:name => "public", :type => "group", :access => "discover"}
     REGISTERED_READ_ACCESS = {:name => "registered", :type => "group", :access => "read"}
+    REGISTERED_DISCOVER_ACCESS = {:name => "registered", :type => "group", :access => "discover"}
 
     READER_GROUP_ACCESS = {:name => READER_GROUP_NAME, :type => "group", :access => "read"}
     EDITOR_GROUP_ACCESS = {:name => EDITOR_GROUP_NAME, :type => "group", :access => "edit"}
