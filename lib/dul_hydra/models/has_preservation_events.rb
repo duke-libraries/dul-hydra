db94024c3271b1d028a55547c5b912fcee878ba6
dchandekstark <dchandekstark@gmail.com>
2013-01-28 14:38:11 -0500

    added #ds_internal_uri; removed #last_fixity_check (use fixity_checks.last); fixed oversight in ::find_by_last_fixity_check; added before_destroy callback and protected method

--- lib/dul_hydra/models/has_preservation_events.rb
+++ lib/dul_hydra/models/has_preservation_events.rb
@@ -7,6 +7,7 @@ module DulHydra::Models
                :property => :is_preservation_event_for, 
                :inbound => true, 
                :class_name => 'PreservationEvent'
+      before_destroy :delete_preservation_events
     end
 
     def validate_checksum(dsID)
@@ -17,18 +18,20 @@ module DulHydra::Models
       PreservationEvent.validate_checksum!(self, dsID)
     end
 
+    # Used to record precise datastream version information.
+    # Fedora's public API retrieves datastream versions by create date (asOfDateTime),
+    # not by datastream version ID.
+    def ds_internal_uri(dsID)
+      "#{internal_uri}/datastreams/#{dsID}?asOfDateTime=#{DulHydra::Utils.ds_as_of_date_time(datastreams[dsID])}" 
+    end
+
     def fixity_checks
-      # preservation_events.select { |e| e.fixity_check? }
       PreservationEvent.find({ActiveFedora::SolrService.solr_name(:is_preservation_event_for, :symbol) => internal_uri}, 
                              {:sort => "#{ActiveFedora::SolrService.solr_name(:event_date_time, :date)} asc"})
     end
 
-    def last_fixity_check
-      fixity_checks.empty? ? nil : fixity_checks.sort_by { |e| e.event_date_time }.last
-    end
-
     def last_fixity_check_to_solr
-      e = last_fixity_check
+      e = fixity_checks.last
       e ? {
         ActiveFedora::SolrService.solr_name(:last_fixity_check_on, :date) => e.event_date_time,
         ActiveFedora::SolrService.solr_name(:last_fixity_check_outcome, :symbol) => e.event_outcome
@@ -38,11 +41,16 @@ module DulHydra::Models
     module ClassMethods
       def find_by_last_fixity_check(before_date=Time.now.utc, limit=100)
         field = ActiveFedora::SolrService.solr_name(:last_fixity_check_on, :date)
-        solr_date = before_date.respond_to?(:strftime) ? PreservationEvent.to_event_date_time : before_date
+        solr_date = before_date.respond_to?(:strftime) ? PreservationEvent.to_event_date_time(before_date) : before_date
         all({:sort => "#{field} asc",
               :rows => limit.to_s,
               :fq => "#{field}:[* TO #{solr_date}]"})
       end
+    end
+
+    protected
+    def delete_preservation_events
+      preservation_events.each { |e| e.delete }
     end
 
   end
