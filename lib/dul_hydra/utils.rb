ac742abcb3cbb832a52daa83d6aa7c8a6f3f3b21
dchandekstark <dchandekstark@gmail.com>
2013-01-28 14:35:39 -0500

    Moved ds_internal_uri to instance method of DulHydra::Models::HasPreservationEvents

--- lib/dul_hydra/utils.rb
+++ lib/dul_hydra/utils.rb
@@ -1,9 +1,7 @@
 module DulHydra::Utils
-  def self.ds_internal_uri(obj, dsID)
-    "#{obj.internal_uri}/datastreams/#{dsID}?asOfDateTime=#{ds_as_of_date_time(obj.datastreams[dsID])}" 
-  end
 
   def self.ds_as_of_date_time(ds)
     ds.dsCreateDate.strftime("%Y-%m-%dT%H:%M:%S.%LZ")
   end
+
 end
