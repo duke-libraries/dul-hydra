efd80719253d8999fc0520222ab2f9fde6d5a3c4
dchandekstark <dchandekstark@gmail.com>
2013-02-05 14:47:38 -0500

    added #components - returns parts

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,9 @@ class Item < DulHydra::Models::Base
 
   has_many :parts, :property => :is_part_of, :inbound => true, :class_name => 'Component'
   belongs_to :collection, :property => :is_member_of, :class_name => 'Collection'
+
+  def components
+    parts
+  end
     
 end
