9d638936c7f62a7ad44fe4330264e753e01d2226
dchandekstark <dchandekstark@gmail.com>
2013-02-08 10:40:27 -0500

    refactored to improve coverage and reduce tests by using expect

--- spec/features/collections/show.html.erb_spec.rb
+++ spec/features/collections/show.html.erb_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "show.html.erb" do
-
   subject { page }
   let(:collection) { FactoryGirl.create(:collection_has_item_has_apo) }
   before { visit collection_path(collection) }
@@ -11,12 +10,17 @@ describe "show.html.erb" do
     collection.reload # work around https://github.com/projecthydra/active_fedora/issues/36
     collection.delete
   end
-  it { should have_content(collection.pid) }
-  it { should have_content(collection.title.first) } 
-  it { should have_content(collection.identifier.first) }
-  it { should have_link("DC") }
-  it { should have_link("RELS-EXT") }
-  it { should have_link("descMetadata") }
-  it { should have_link(collection.items.first.pid, :href => item_path(collection.items.first)) }
-  
+  it "should display the PID, title and identifier" do
+    expect(subject).to have_content(collection.pid)
+    expect(subject).to have_content(collection.title.first)
+    expect(subject).to have_content(collection.identifier.first)
+  end
+  it "should have links to datastreams" do
+    collection.datastreams.each_key do |dsid|
+      expect(subject).to have_link(dsid)
+    end
+  end
+  it "should have links to child objects" do
+    expect(subject).to have_link(collection.items.first.pid, :href => item_path(item.parts.first)) 
+  end
 end
