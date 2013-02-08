d8d0839102f554c012c8bdf316025f80c5ba5b5d
dchandekstark <dchandekstark@gmail.com>
2013-02-08 15:26:52 -0500

    removed extra method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,11 @@
 module ApplicationHelper
 
   def object_datastreams_path(obj)
-    send "#{object_path_prefix(obj)}_datastreams_path".to_sym, obj
+    send "#{obj.class.to_s.downcase}_datastreams_path".to_sym, obj
   end
 
   def object_datastream_path(obj, dsid)
-    send "#{object_path_prefix(obj)}_datastream_path".to_sym, obj, dsid
-  end
-
-  def object_path_prefix(obj)
-    obj.class.to_s.downcase
+    send "#{obj.class.to_s.downcase}_datastream_path".to_sym, obj, dsid
   end
 
 end
