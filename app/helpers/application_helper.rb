15ded7c71a57965b2ab50a5392886d7c3558c717
dchandekstark <dchandekstark@gmail.com>
2013-02-08 15:25:35 -0500

    revised to use path helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
+  def object_datastreams_path(obj)
+    send "#{object_path_prefix(obj)}_datastreams_path".to_sym, obj
+  end
+
   def object_datastream_path(obj, dsid)
-    "/#{obj.class.to_s.downcase.pluralize}/#{obj.pid}/datastreams/#{dsid}"
+    send "#{object_path_prefix(obj)}_datastream_path".to_sym, obj, dsid
+  end
+
+  def object_path_prefix(obj)
+    obj.class.to_s.downcase
   end
 
 end
