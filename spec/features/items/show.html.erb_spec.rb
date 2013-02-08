08f6ce1d69d0677dea5d2b57692bd57cd699a48f
dchandekstark <dchandekstark@gmail.com>
2013-02-08 10:40:52 -0500

    refactored to improve coverage and reduce tests by using expect

--- spec/features/items/show.html.erb_spec.rb
+++ spec/features/items/show.html.erb_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "show.html.erb" do
-
   subject { page }
   let(:item) { FactoryGirl.create(:item_in_collection_has_part_has_apo) }
   before { visit item_path(item) }
@@ -12,13 +11,18 @@ describe "show.html.erb" do
     item.reload # work around https://github.com/projecthydra/active_fedora/issues/36
     item.delete
   end
-  it { should have_content(item.pid) }
-  it { should have_content(item.title.first) } 
-  it { should have_content(item.identifier.first) }
-  it { should have_link("DC") }
-  it { should have_link("RELS-EXT") }
-  it { should have_link("descMetadata") }
-  it { should have_link(item.parts.first.pid, :href => component_path(item.parts.first)) }
-  it { should have_link(item.collection.pid, :href => collection_path(item.collection)) }
-  
+  it "should display the PID, title and identifier" do
+    expect(subject).to have_content(item.pid)
+    expect(subject).to have_content(item.title.first)
+    expect(subject).to have_content(item.identifier.first)
+  end
+  it "should have links to datastreams" do
+    item.datastreams.each_key do |dsid|
+      expect(subject).to have_link(dsid)
+    end
+  end
+  it "should have links to parent and child objects" do
+    expect(subject).to have_link(item.parts.first.pid, :href => item_path(item.parts.first)) 
+    expect(subject).to have_link(item.collection.pid, :href => collection_path(item.collection))
+  end
 end
