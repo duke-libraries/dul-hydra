398954d32fa896fb55a9018feb317ff1a0aba3f8
dchandekstark <dchandekstark@gmail.com>
2013-02-08 10:11:16 -0500

    added names to datastream routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ DulHydra::Application.routes.draw do
   # DulHydra objects
   [:collections, :items, :components].each do |model|
     resources model
-    match "/#{model}/:id/datastreams" => "#{model}#datastreams", :via => :get
-    match "/#{model}/:id/datastreams/:dsid" => "#{model}#datastream", :via => :get
-    match "/#{model}/:id/datastreams/:dsid/content" => "#{model}#datastream_content", :via => :get
+    match "/#{model}/:id/datastreams" => "#{model}#datastreams", :via => :get, :as => "#{model}_datastreams"
+    match "/#{model}/:id/datastreams/:dsid" => "#{model}#datastream", :via => :get, :as => "#{model}_datastream"
+    match "/#{model}/:id/datastreams/:dsid/content" => "#{model}#datastream_content", :via => :get, :as => "#{model}_datastream_content"
   end
   
 end
