00dc44144a0e43390971ecc0591929ad77690905
dchandekstark <dchandekstark@gmail.com>
2013-02-08 15:44:54 -0500

    refactored to use shared examples

--- spec/features/components/datastreams.html.erb_spec.rb
+++ spec/features/components/datastreams.html.erb_spec.rb
@@ -1,16 +1,6 @@
 require 'spec_helper'
+require 'support/shared_examples_for_dul_hydra_views'
 
 describe "datastreams.html.erb" do
-  subject { page }
-  let(:component) { FactoryGirl.create(:component_has_apo) }
-  before { visit component_datastreams_path(component) }
-  after do
-    component.admin_policy.delete
-    component.delete
-  end
-  it "should have links to all datastreams" do
-    component.datastreams.each_key do |dsid|
-      expect(subject).to have_link(dsid, :href => component_datastream_path(component, dsid))
-    end
-  end
+  it_behaves_like "a DulHydra object datastreams view", :component
 end
