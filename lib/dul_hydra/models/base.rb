2e5d696a932d600eaed10e5f5d139d0047b746c2
dchandekstark <dchandekstark@gmail.com>
2013-02-13 14:35:49 -0500

    added #title_display index title_display

--- lib/dul_hydra/models/base.rb
+++ lib/dul_hydra/models/base.rb
@@ -6,10 +6,17 @@ module DulHydra::Models
     include AccessControllable
     include HasPreservationEvents
 
+    NO_TITLE = "[No Title]"
+
     def to_solr(solr_doc=Hash.new, opts={})
       solr_doc = super(solr_doc, opts)
       solr_doc.merge!(last_fixity_check_to_solr)
+      solr_doc.merge!(:title_display => title_display)
       solr_doc
+    end
+
+    def title_display
+      title.first || NO_TITLE
     end
 
   end
