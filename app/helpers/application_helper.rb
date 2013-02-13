acdc384909375a6ac1c0b5dd229b34e89067c1a5
dchandekstark <dchandekstark@gmail.com>
2013-02-13 15:51:26 -0500

    added #internal_uri_to_pid marked object_datastream_* methods as deprecated w/comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,20 @@
 module ApplicationHelper
 
+  def internal_uri_to_pid(args)
+    args[:document][args[:field]].first.split('/').last
+  end
+
+  # DEPRECATED
   def object_datastreams_path(obj)
     send "#{obj.class.to_s.downcase}_datastreams_path".to_sym, obj
   end
 
+  # DEPRECATED
   def object_datastream_path(obj, dsid)
     send "#{obj.class.to_s.downcase}_datastream_path".to_sym, obj, dsid
   end
 
+  # DEPRECATED
   def object_datastream_content_path(obj, dsid)
     send "#{obj.class.to_s.downcase}_datastream_content_path".to_sym, obj, dsid
   end
