f5fe808f3e158f7ed7c8d3ebdf9a7ecfd0b1bf37
dchandekstark <dchandekstark@gmail.com>
2013-02-15 15:42:18 -0500

    emptied module

--- lib/dul_hydra/controllers/controller_behavior.rb
+++ lib/dul_hydra/controllers/controller_behavior.rb
@@ -1,13 +1,5 @@
 module DulHydra::Controllers
   module ControllerBehavior
 
-    def model_class
-      self.class.name.sub("Controller", "").singularize
-    end
-
-    def model_instance_variable_get
-      self.instance_variable_get("@#{model_class.downcase}")
-    end
-
   end
 end
