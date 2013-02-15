cbcb941e990b6356ef72e210b702987b1edb40e5
dchandekstark <dchandekstark@gmail.com>
2013-02-15 18:35:58 -0500

    refactored

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -1,26 +1,96 @@
 require 'spec_helper'
 
 describe CatalogController do
-  subject { get :show, :id => item }
-  let(:item) { FactoryGirl.create(:item) }
-  let(:apo) { FactoryGirl.create(:public_read_policy) }
-  before do
-    item.admin_policy = apo
-    item.save!
-    sign_in user
+
+  let(:user) { FactoryGirl.create(:user) }
+  before(:each) { sign_in user }
+  after(:all) { user.delete }
+
+  context "#model_index" do
+    subject { get :model_index, :model => object.class.to_s }
+    after { object.delete }
+    context "Collection" do
+      let(:object) { FactoryGirl.create(:collection_public_read) }
+      it { should render_template(:model_index) }
+    end
+    context "Item" do
+      let(:object) { FactoryGirl.create(:item_public_read) }
+      it { should render_template(:model_index) }
+    end
+    context "Component" do
+      let(:object) { FactoryGirl.create(:component_public_read) }
+      it { should render_template(:model_index) }
+    end
   end
-  after do
-    sign_out user
-    user.delete
-    apo.delete
-    item.delete
+
+  context "#show" do
+    subject { get :show, :id => object }
+    after { object.delete }
+    context "Collection" do
+      let(:object) { FactoryGirl.create(:collection_public_read) }
+      it { should render_template(:show) }
+    end
+    context "Item" do
+      let(:object) { FactoryGirl.create(:item_public_read) }
+      it { should render_template(:show) }
+    end
+    context "Component" do
+      let(:object) { FactoryGirl.create(:component_public_read) }
+      it { should render_template(:show) }
+    end
   end
-  context "registered user" do
-    let(:user) { FactoryGirl.create(:user) }
-    it { should render_template(:show) }
+
+  context "#datastreams" do
+    subject { get :datastreams, :object_id => object }
+    after { object.delete }
+    context "Collection" do
+      let(:object) { FactoryGirl.create(:collection_public_read) }
+      it { should render_template(:datastreams) }
+    end
+    context "Item" do
+      let(:object) { FactoryGirl.create(:item_public_read) }
+      it { should render_template(:datastreams) }
+    end
+    context "Component" do
+      let(:object) { FactoryGirl.create(:component_public_read) }
+      it { should render_template(:datastreams) }
+    end
   end
-  context "editor" do
-    let(:user) { FactoryGirl.create(:editor) }
-    it { should render_template(:show) }
+
+  context "#datastream" do
+    subject { get :datastream, :object_id => object, :id => dsid }
+    after { object.delete }
+    let(:dsid) { "DC" }
+    context "Collection" do
+      let(:object) { FactoryGirl.create(:collection_public_read) }
+      it { should render_template(:datastream) }
+    end
+    context "Item" do
+      let(:object) { FactoryGirl.create(:item_public_read) }
+      it { should render_template(:datastream) }
+    end
+    context "Component" do
+      let(:object) { FactoryGirl.create(:component_public_read) }
+      it { should render_template(:datastream) }
+    end
+  end
+
+  context "#datastream_content" do
+    subject { get :datastream_content, :object_id => object, :id => dsid }
+    after { object.delete }
+    let(:dsid) { "DC" }
+    context "Collection" do
+      let(:object) { FactoryGirl.create(:collection_public_read) }
+      it { should be_successful }
+    end
+    context "Item" do
+      let(:object) { FactoryGirl.create(:item_public_read) }
+      it { should be_successful }
+    end
+    context "Component" do
+      let(:object) { FactoryGirl.create(:component_public_read) }
+      it { should be_successful }
+    end
+    it "should download non-text content as attachment"
   end
 end
