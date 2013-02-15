ef6597be55d2567a1dc60732b0a49d48a3874f49
Jim Coble <jim.coble@duke.edu>
2013-02-15 13:22:05 -0500

    work in progress

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -8,6 +8,7 @@ class Collection < DulHydra::Models::Base
   include DulHydra::Models::HasTripodMets
 
   has_many :items, :property => :is_member_of, :inbound => true, :class_name => 'Item'
+  has_many :targets, :property => :is_external_target_for, :inbound => true, :class_name => 'Target'
 
   alias_method :children, :items
   
--- app/models/target.rb
+++ app/models/target.rb
@@ -3,6 +3,7 @@ class Target < DulHydra::Models::Base
   include DulHydra::Models::HasContent
   include DulHydra::Models::HasJhove
 
-  has_many :components, :property => :is_external_target_for, :inbound => true, :class_name => 'Component'
+  has_many :components, :property => :has_external_target, :inbound => true, :class_name => 'Component'
+  belongs_to :collection, :property => :is_external_target_for, :class_name => 'Collection'
   
 end
