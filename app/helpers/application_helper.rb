e1698ac3289a185350defbc35087bd2e46a95da2
dchandekstark <dchandekstark@gmail.com>
2013-02-15 10:29:21 -0500

    removed deprecated methods added #uri_to_pid

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,7 @@
 module ApplicationHelper
 
-  # DEPRECATED
-  def object_datastreams_path(obj)
-    send "#{obj.class.to_s.downcase}_datastreams_path".to_sym, obj
-  end
-
-  # DEPRECATED
-  def object_datastream_path(obj, dsid)
-    send "#{obj.class.to_s.downcase}_datastream_path".to_sym, obj, dsid
-  end
-
-  # DEPRECATED
-  def object_datastream_content_path(obj, dsid)
-    send "#{obj.class.to_s.downcase}_datastream_content_path".to_sym, obj, dsid
+  def uri_to_pid(uri)
+    uri.split('/').last
   end
 
 end
