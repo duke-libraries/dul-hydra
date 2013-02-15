83d781f6b3bdb62791bb7a059cf4aade42f41c6b
dchandekstark <dchandekstark@gmail.com>
2013-02-15 12:36:07 -0500

    added methods

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -9,11 +9,11 @@ class SolrDocument
   # self.unique_key = 'id'
   
   # The following shows how to setup this blacklight document to display marc documents
-  extension_parameters[:marc_source_field] = :marc_display
-  extension_parameters[:marc_format_type] = :marcxml
-  use_extension( Blacklight::Solr::Document::Marc) do |document|
-    document.key?( :marc_display  )
-  end
+  # extension_parameters[:marc_source_field] = :marc_display
+  # extension_parameters[:marc_format_type] = :marcxml
+  # use_extension( Blacklight::Solr::Document::Marc) do |document|
+  #   document.key?( :marc_display  )
+  # end
   
   # Email uses the semantic field mappings below to generate the body of an email.
   SolrDocument.use_extension( Blacklight::Solr::Document::Email )
@@ -34,6 +34,10 @@ class SolrDocument
                          :format => "format"
                          )
 
+  #
+  # Custom methods
+  #
+
   def object_profile
     JSON.parse(self[:object_profile_display].first)
   end
@@ -42,12 +46,30 @@ class SolrDocument
     object_profile["datastreams"]
   end
 
+  def admin_policy?
+    !admin_policy_uri.nil?
+  end
+
   def admin_policy_uri
     get(:is_governed_by_s)
   end
 
+  def admin_policy_pid
+    uri = admin_policy_uri
+    uri &&= ActiveFedora::Base.pids_from_uris(uri)
+  end
+
+  def parent?
+    !parent_uri.nil?
+  end
+
   def parent_uri
     get(:is_part_of_s) || get(:is_member_of_s) || get(:is_member_of_collection_s)
+  end
+
+  def parent_pid
+    uri = parent_uri
+    uri &&= ActiveFedora::Base.pids_from_uris(uri)
   end
 
 end
