32d8371e0d60b2734a2417a493f8fb894b7c9743
dchandekstark <dchandekstark@gmail.com>
2013-02-21 15:41:27 -0500

    audit trail node now <audit:auditTrail>

--- lib/dul_hydra/models/auditable.rb
+++ lib/dul_hydra/models/auditable.rb
@@ -7,16 +7,18 @@ module DulHydra::Models
     
     private
     
-    AUDIT_TRAIL_NS = {'audit' => 'info:fedora/fedora-system:def/audit#'}
+    AT_NS = {'audit' => 'info:fedora/fedora-system:def/audit#'}
+    FOXML_NS = {'foxml' => 'info:fedora/fedora-system:def/foxml#'}
+    AT_XPATH = '/foxml:digitalObject/foxml:datastream[@ID = "AUDIT"]/descendant::audit:auditTrail'
     
     class FedoraAuditTrail
       def initialize(object)
-        @ng_xml = Nokogiri::XML(object.inner_object.repository.object_xml(:pid => object.pid)).xpath('/foxml:digitalObject/foxml:datastream[@ID = "AUDIT"]')  
+        @ng_xml = Nokogiri::XML(object.inner_object.repository.object_xml(:pid => object.pid)).xpath(AT_XPATH, FOXML_NS.merge(AT_NS))  
       end
       def records
         if !@records
           @records = []
-          @ng_xml.xpath('.//audit:record', AUDIT_TRAIL_NS).each do |node| 
+          @ng_xml.xpath('.//audit:record', AT_NS).each do |node| 
             @records << FedoraAuditRecord.new(node)
           end
         end
@@ -35,22 +37,22 @@ module DulHydra::Models
         @record['ID']
       end
       def process
-        @record.at_xpath('audit:process/@type', AUDIT_TRAIL_NS).text
+        @record.at_xpath('audit:process/@type', AT_NS).text
       end
       def action
-        @record.at_xpath('audit:action', AUDIT_TRAIL_NS).text
+        @record.at_xpath('audit:action', AT_NS).text
       end
       def component_id
-        @record.at_xpath('audit:componentID', AUDIT_TRAIL_NS).text
+        @record.at_xpath('audit:componentID', AT_NS).text
       end
       def responsibility
-        @record.at_xpath('audit:responsibility', AUDIT_TRAIL_NS).text
+        @record.at_xpath('audit:responsibility', AT_NS).text
       end
       def date
-        @record.at_xpath('audit:date', AUDIT_TRAIL_NS).text
+        @record.at_xpath('audit:date', AT_NS).text
       end
       def justification
-        @record.at_xpath('audit:justification', AUDIT_TRAIL_NS).text
+        @record.at_xpath('audit:justification', AT_NS).text
       end
     end    
         
