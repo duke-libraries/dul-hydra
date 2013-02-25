c57f8618b23434b313211966c3eaea5c541526a9
dchandekstark <dchandekstark@gmail.com>
2013-02-25 16:01:32 -0500

    reduced unnecessary test iterations

--- spec/support/shared_examples_for_preservation_events.rb
+++ spec/support/shared_examples_for_preservation_events.rb
@@ -1,25 +1,31 @@
 require 'spec_helper'
 
 shared_examples "a preservation event having a success outcome" do
-  its(:event_outcome) { should eq(PreservationEvent::SUCCESS) }
-  its(:success?) { should be_true }
-  its(:failure?) { should be_false }
+  it "should behave like a success event" do
+    subject.event_outcome.should eq(PreservationEvent::SUCCESS)
+    subject.success?.should be_true
+    subject.failure?.should be_false
+  end
 end
 
 shared_examples "a preservation event having a failure outcome" do
-  its(:event_outcome) { should eq(PreservationEvent::FAILURE) }
-  its(:success?) { should be_false }
-  its(:failure?) { should be_true }
+  it "should behave like a failure event" do
+    subject.event_outcome.should eq(PreservationEvent::FAILURE)
+    subject.success?.should be_false
+    subject.failure?.should be_true
+  end
 end
 
 shared_examples "a fixity check preservation event" do
   its(:for_object) { should be_kind_of(DulHydra::Models::HasPreservationEvents) }
-  its(:fixity_check?) { should be_true }
-  its(:event_date_time) { should_not be_nil }
-  its(:event_type) { should eq(PreservationEvent::FIXITY_CHECK) }
-  its(:event_id_type) { should eq(PreservationEvent::UUID) }
-  its(:event_id_value) { should_not be_nil }
-  its(:linking_object_id_type) { should eq(PreservationEvent::DATASTREAM) }
+  it "should behave like a fixity check" do
+    subject.fixity_check?.should be_true
+    subject.event_date_time.should_not be_nil
+    subject.event_type.should eq(PreservationEvent::FIXITY_CHECK)
+    subject.event_id_type.should eq(PreservationEvent::UUID)
+    subject.event_id_value.should_not be_nil
+    subject.linking_object_id_type.should eq(PreservationEvent::DATASTREAM)
+  end
 end
 
 shared_examples "a fixity check success preservation event" do
