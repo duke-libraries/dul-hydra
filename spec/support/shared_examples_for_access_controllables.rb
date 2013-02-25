efbc43a239b60b5fe6f29c4865846dd9fa10adaa
dchandekstark <dchandekstark@gmail.com>
2013-02-25 16:01:41 -0500

    cleanup

--- spec/support/shared_examples_for_access_controllables.rb
+++ spec/support/shared_examples_for_access_controllables.rb
@@ -1,3 +1,6 @@
 shared_examples "an access controllable object" do
-  it "should have a rightMetadata datastream"
+  it "should have a rightsMetadata datastream" do
+    subject.datastreams.keys.should include("rightsMetadata")
+    subject.datastreams["rightsMetadata"].should be_kind_of(Hydra::Datastream::RightsMetadata)
+  end
 end
--- spec/support/shared_examples_for_has_content.rb
+++ spec/support/shared_examples_for_has_content.rb
@@ -1,20 +1,10 @@
 require 'spec_helper'
 
 shared_examples "an object that has content" do
-
   context "new" do
     let(:obj) { described_class.new }
     it "should have a content datastream" do
       obj.datastreams["content"].should be_kind_of(DulHydra::Datastreams::FileContentDatastream)
     end
   end
-
-  context "#validate_content_checksum" do
-
-  end
-
-  context "#validate_content_checksum!" do
-
-  end
-
 end
