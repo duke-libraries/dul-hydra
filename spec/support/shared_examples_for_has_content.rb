d9f1c34ec967b492e0c5182c0891eac94fc7297a
dchandekstark <dchandekstark@gmail.com>
2013-02-26 15:15:30 -0500

    added tests

--- spec/support/shared_examples_for_has_content.rb
+++ spec/support/shared_examples_for_has_content.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
+require 'tempfile'
 
 shared_examples "an object that has content" do
-  context "new" do
-    let(:obj) { described_class.new }
-    it "should have a content datastream" do
-      obj.datastreams["content"].should be_kind_of(DulHydra::Datastreams::FileContentDatastream)
+  let!(:object) { described_class.create! }
+  let(:file_path) { File.join(File.dirname(__FILE__), '..', 'fixtures', 'library-devil.tiff') }
+  before do
+    object.content.content_file = File.new(file_path, "rb")
+    object.save!
+  end
+  after { object.delete }
+  context "content datastream" do
+    context "#content_file=" do
+      it "should store the file content" do
+        object.content.content.size.should == File.size(file_path)
+      end
+    end
+    context "defaults" do
+      it "should have a default file prefix, file extension, and file name" do
+        pid_prefix = object.pid.sub(':', '_')
+        object.content.default_file_prefix.should == "#{pid_prefix}_content"
+        object.content.default_file_extension.should == "tiff"
+        object.content.default_file_name.should == "#{pid_prefix}_content.tiff"
+      end
+    end
+    context "#write_content" do
+      it "should write the content to a file" do
+        tmpfile = Tempfile.new('content', :encoding => 'ascii-8bit')
+        tmppath = tmpfile.path
+        object.content.write_content(tmpfile)
+        tmpfile.close
+        object.content.content.size.should == File.size(tmppath)
+      end
     end
   end
 end
