ec9b789fb74b13c3656221bb08412d0901a15203
dchandekstark <dchandekstark@gmail.com>
2013-02-27 15:33:58 -0500

    work in progress

--- app/controllers/export_sets_controller.rb
+++ app/controllers/export_sets_controller.rb
@@ -16,19 +16,20 @@ class ExportSetsController < ApplicationController
   end
   
   def new
-    @export_set = ExportSet.new
+    @export_set = ExportSet.new   
     bookmark_ids = current_user.bookmarks.collect { |b| b.document_id.to_s }
     @response, @document_list = get_solr_response_for_field_values(SolrDocument.unique_key, bookmark_ids)
     # hack to filter just Components
     # would be nice to filter DulHydra::Models::HasContent
     @document_list.keep_if { |doc| doc.get(:active_fedora_model_s) == 'Component' }
-    if @document_list.empty?
+    if @document_list.empty
       flash[:notice] = "You have no bookmarks for content-bearing objects."
     end
   end
   
   def create
     @export_set = ExportSet.new(params[:export_set])
+    @export_set.user = current_user
     Dir.mktmpdir do |tmpdir|
       # create the zip archive
       zip_name = "export_set_#{Time.now.strftime('%Y%m%d%H%M%S')}.zip"
@@ -57,7 +58,7 @@ class ExportSetsController < ApplicationController
     redirect_to :action => :show, :id => @export_set
   end
   
-  def delete
+  def destroy
     @export_set = ExportSet.find(params[:id])
     @export_set.destroy
     flash[:notice] = "Export Set destroyed."
