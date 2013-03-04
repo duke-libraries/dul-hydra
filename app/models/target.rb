a3a5d98abd4c03ab281577528022777af2959e9a
Jim Coble <jim.coble@duke.edu>
2013-03-04 16:00:34 -0500

    enable targets to have thumbnails

--- app/models/target.rb
+++ app/models/target.rb
@@ -2,6 +2,7 @@ class Target < DulHydra::Models::Base
 
   include DulHydra::Models::HasContent
   include DulHydra::Models::HasJhove
+  include DulHydra::Models::HasThumbnail
 
   has_many :components, :property => :has_external_target, :inbound => true, :class_name => 'Component'
   belongs_to :collection, :property => :is_external_target_for, :class_name => 'Collection'
