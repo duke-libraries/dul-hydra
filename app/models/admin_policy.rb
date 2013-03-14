5d2bc1817cd21cd6140af4e0ec10a255f21822a8
dchandekstark <dchandekstark@gmail.com>
2013-03-14 17:58:49 -0400

    Closes #151

--- app/models/admin_policy.rb
+++ app/models/admin_policy.rb
@@ -4,6 +4,10 @@
 #
 class AdminPolicy < Hydra::AdminPolicy
 
+  delegate :default_license_title, :to => 'defaultRights', :at => [:license, :title], :unique => true
+  delegate :default_license_description, :to => 'defaultRights', :at => [:license, :description], :unique => true
+  delegate :default_license_url, :to => 'defaultRights', :at => [:license, :url], :unique => true
+
   APO_NAMESPACE = "duke-apo"
 
   def self.create_pid(suffix)
--- spec/models/admin_policy_spec.rb
+++ spec/models/admin_policy_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe AdminPolicy do
+  context "terms delegated to defaultRights" do
+    let(:apo) { AdminPolicy.new }
+    before do
+      apo.default_license_title = "License Title"
+      apo.default_license_description = "License Description"
+      apo.default_license_url = "http://library.duke.edu"
+    end
+    it "should set the terms correctly" do
+      apo.defaultRights.license.title.first.should == "License Title"
+      apo.defaultRights.license.description.first.should == "License Description"
+      apo.defaultRights.license.url.first.should == "http://library.duke.edu"
+    end
+  end
 end
