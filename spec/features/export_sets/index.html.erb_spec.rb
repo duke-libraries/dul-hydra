d1bdd5c1d166236c54587a411f0e9aa6ff9ae61f
dchandekstark <dchandekstark@gmail.com>
2013-03-18 18:05:31 -0400

    added tests and improved views

--- spec/features/export_sets/index.html.erb_spec.rb
+++ spec/features/export_sets/index.html.erb_spec.rb
@@ -3,24 +3,41 @@ require 'helpers/user_helper'
 
 describe "export_sets/index.html.erb" do
   let!(:user) { FactoryGirl.create(:user) }
-  before do
-    login user
-    visit export_sets_path
-  end
+  before { login user }
   after { user.delete }
   context "user has no bookmarks" do
     it "should not have a 'new export set' link" do
+      visit export_sets_path
       page.should_not have_link("New Export Set")
     end
   end
   context "user has bookmarks" do
-    pending
+    let(:object) { FactoryGirl.create(:test_content) }
+    after { object.delete }
+    it "should have a 'new export set' link" do
+      visit catalog_path(object)
+      click_button "bookmark_toggle_#{object.pid.sub(/:/, '-')}"
+      visit export_sets_path
+      page.should have_link("New Export Set")
+    end    
   end
   context "user has an existing export set" do
-    pending
+    let(:object) { FactoryGirl.create(:test_content) }
+    let(:export_set) { ExportSet.new }
+    before do
+      export_set.user = user
+      export_set.pids = [object.pid]
+      export_set.create_archive
+    end
+    after { object.delete }
+    it "should list the export set" do
+      visit export_sets_path
+      page.should have_link(export_set.id, :href => export_set_path(export_set))
+    end        
   end
   context "user has no export sets" do
     it "should display a 'no export sets' flash notice" do
+      visit export_sets_path
       page.should have_content("no export sets")
     end
   end
--- spec/features/export_sets/show.html.erb_spec.rb
+++ spec/features/export_sets/show.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
+require 'helpers/user_helper'
 
 describe "export_sets/show.html.erb" do
-  it "should display information about the export set"
+  let(:object) { FactoryGirl.create(:test_content) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:export_set) { ExportSet.new }
+  before do
+    login user
+    export_set.title = "Test Export Set"
+    export_set.user = user
+    export_set.pids = [object.pid]
+    export_set.create_archive
+  end
+  after do
+    object.delete 
+    user.delete
+  end
+  it "should display information about the export set" do
+    visit export_set_path(export_set)
+    page.should have_content(export_set.title)
+  end
 end
