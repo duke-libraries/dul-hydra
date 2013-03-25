2c4b0902a8503bbe7cbae6fde07953a608b01664
dchandekstark <dchandekstark@gmail.com>
2013-03-25 14:16:56 -0700

    added comment

--- lib/dul_hydra/helpers/catalog_helper_behavior.rb
+++ lib/dul_hydra/helpers/catalog_helper_behavior.rb
@@ -7,6 +7,8 @@ module DulHydra::Helpers
 
     def internal_uri_to_link(args)
       pid = internal_uri_to_pid(args).first
+      # Depends on Blacklight::SolrHelper#get_solr_response_for_doc_id 
+      # having been added as a helper method to CatalogController
       response, doc = get_solr_response_for_doc_id(pid)
       title = doc.nil? ? pid : doc.fetch('title_display', pid)
       link_to(title, catalog_path(pid), :class => "parent-link").html_safe
