069000d754f6d80c56697ec07aef8de0c9beb90a
Jim Coble <jim.coble@duke.edu>
2013-04-12 11:26:23 -0400

    fix failing test by parenthesizing OR'ed conditions

--- lib/dul_hydra/solr_helper.rb
+++ lib/dul_hydra/solr_helper.rb
@@ -20,7 +20,7 @@ module DulHydra::SolrHelper
   def children_filter(solr_params, user_params)
     solr_params[:fq] ||= []
     object_uri = ActiveFedora::SolrService.escape_uri_for_query("info:fedora/#{user_params[:object_id]}")
-    solr_params[:fq] << "+#{ActiveFedora::SolrService.solr_name(:is_member_of, :symbol)}:#{object_uri} OR #{ActiveFedora::SolrService.solr_name(:is_member_of_collection, :symbol)}:#{object_uri} OR #{ActiveFedora::SolrService.solr_name(:is_part_of, :symbol)}:#{object_uri}"
+    solr_params[:fq] << "+(#{ActiveFedora::SolrService.solr_name(:is_member_of, :symbol)}:#{object_uri} OR #{ActiveFedora::SolrService.solr_name(:is_member_of_collection, :symbol)}:#{object_uri} OR #{ActiveFedora::SolrService.solr_name(:is_part_of, :symbol)}:#{object_uri})"
     # FIXME re-enable sort
     #solr_params[:sort] = ["title_display_sort asc"]
   end
