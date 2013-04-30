3ee16d8987672a533b7df6ec506b979a5446b029
dchandekstark <dchandekstark@gmail.com>
2013-04-30 10:01:54 -0400

    added INTERNAL_URI

--- lib/dul_hydra/index_fields.rb
+++ lib/dul_hydra/index_fields.rb
@@ -9,6 +9,7 @@ module DulHydra::IndexFields
   EVENT_ID_VALUE = ActiveFedora::SolrService.solr_name(:event_id_value, :symbol)
   EVENT_TYPE = ActiveFedora::SolrService.solr_name(:event_type, :symbol)
   IDENTIFIER = ActiveFedora::SolrService.solr_name(:identifier, :stored_searchable, type: :text)
+  INTERNAL_URI = ActiveFedora::SolrService.solr_name(:internal_uri, :symbol)
   IS_EXTERNAL_TARGET_FOR = ActiveFedora::SolrService.solr_name(:is_external_target_for, :symbol)
   IS_GOVERNED_BY = ActiveFedora::SolrService.solr_name(:is_governed_by, :symbol)
   IS_MEMBER_OF = ActiveFedora::SolrService.solr_name(:is_member_of, :symbol)
