846a26257c04f2f0e4d9866cb195e3bcf1a3e41c
dchandekstark <dchandekstark@gmail.com>
2013-05-13 15:27:16 -0400

    Updated preservation_events_filter to work with fcrepo_admin scoped route

--- lib/dul_hydra/solr_helper.rb
+++ lib/dul_hydra/solr_helper.rb
@@ -7,14 +7,7 @@ module DulHydra::SolrHelper
 
   def preservation_events_filter(solr_params, user_params)
     solr_params[:fq] ||= []
-    solr_params[:fq] << "+#{DulHydra::IndexFields::IS_PRESERVATION_EVENT_FOR}:\"info:fedora/#{user_params[:id]}\""
-  end
-
-  def children_filter(solr_params, user_params)
-    solr_params[:fq] ||= []
-    object_uri = ActiveFedora::SolrService.escape_uri_for_query("info:fedora/#{user_params[:object_id]}")
-    solr_params[:fq] << "+(#{DulHydra::IndexFields::IS_MEMBER_OF}:#{object_uri} OR #{DulHydra::IndexFields::IS_MEMBER_OF_COLLECTION}:#{object_uri} OR #{DulHydra::IndexFields::IS_PART_OF}:#{object_uri})"
-    solr_params[:sort] = ["#{DulHydra::IndexFields::TITLE} asc"]
+    solr_params[:fq] << "+#{DulHydra::IndexFields::IS_PRESERVATION_EVENT_FOR}:\"info:fedora/#{user_params[:object_id]}\""
   end
 
 end
