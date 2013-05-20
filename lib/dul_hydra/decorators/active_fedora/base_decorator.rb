627600e804507308f4ef40c600a0ca09c28f62d0
dchandekstark <dchandekstark@gmail.com>
2013-05-20 13:39:46 -0400

    #has_content? moved to ActiveFedora::Base decorator

--- lib/dul_hydra/decorators/active_fedora/base_decorator.rb
+++ lib/dul_hydra/decorators/active_fedora/base_decorator.rb
@@ -4,4 +4,8 @@ ActiveFedora::Base.class_eval do
     self.is_a?(DulHydra::Models::HasPreservationEvents) && self.preservation_events.count > 0
   end
 
+  def has_content?
+    self.is_a?(DulHydra::Models::HasContent) && self.datastreams[DulHydra::Datastreams::CONTENT].has_content?
+  end
+
 end
