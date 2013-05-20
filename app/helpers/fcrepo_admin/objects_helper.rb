6cdb88a60e6539cc0ff748a828f1f4df8e06e41f
dchandekstark <dchandekstark@gmail.com>
2013-05-20 13:40:30 -0400

    New export set template updated

--- app/helpers/fcrepo_admin/objects_helper.rb
+++ app/helpers/fcrepo_admin/objects_helper.rb
@@ -12,12 +12,12 @@ module FcrepoAdmin
       solr_doc.get(DulHydra::IndexFields.const_get(field.to_s.upcase))
     end
 
+    def render_bookmark_control
+      render :partial => 'fcrepo_admin/catalog/bookmark_control', :locals => {:object => @object}
+    end
+
     def custom_object_nav_item(item)
       case
-      when item == :bookmark
-        if current_user
-          render :partial => 'fcrepo_admin/catalog/bookmark_control', :locals => {:object => @object}
-        end
       when item == :preservation_events
         if @object.has_preservation_events?
           link_to_unless_current t("fcrepo_admin.object.nav.items.preservation_events"), preservation_events_path(@object)
--- config/initializers/fcrepo_admin.rb
+++ config/initializers/fcrepo_admin.rb
@@ -1,8 +1,8 @@
 module DulHydra
   class Application < Rails::Application
     config.before_initialize do
-      #FcrepoAdmin.read_only = true
-      FcrepoAdmin.object_nav_items = [:pid, :bookmark, :summary, :datastreams, :permissions, :associations, :preservation_events, :audit_trail]
+      FcrepoAdmin.read_only = true
+      FcrepoAdmin.object_nav_items = [:pid, :summary, :datastreams, :permissions, :associations, :preservation_events, :audit_trail]
     end
   end
 end
