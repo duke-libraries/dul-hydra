4f93a6093263478ca2ab189397d6c262ff272d7c
dchandekstark <dchandekstark@gmail.com>
2013-05-23 14:26:56 -0400

    Updated ThumbnailController

--- app/controllers/thumbnail_controller.rb
+++ app/controllers/thumbnail_controller.rb
@@ -3,7 +3,7 @@ class ThumbnailController < ApplicationController
   def show
     @object = ActiveFedora::Base.find(params[:object_id], :cast => true)
     # skip authz
-    if @object.is_a?(DulHydra::Models::HasThumbnail) && @object.has_thumbnail?
+    if @object.has_thumbnail?
       send_data @object.thumbnail.content, :type => @object.thumbnail.mimeType
     else
       render :text => 'Not Found', :status => 404
