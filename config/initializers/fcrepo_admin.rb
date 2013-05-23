a26885657245a57b703016540420887340fdeba9
dchandekstark <dchandekstark@gmail.com>
2013-05-23 10:35:06 -0400

    Added associated objects sort setting; upgraded fcrepo_admin to 0.5.3

--- config/initializers/fcrepo_admin.rb
+++ config/initializers/fcrepo_admin.rb
@@ -3,6 +3,7 @@ module DulHydra
     config.before_initialize do
       FcrepoAdmin.read_only = true
       FcrepoAdmin.object_nav_items = [:pid, :summary, :datastreams, :permissions, :children, :associations, :preservation_events, :audit_trail, :object_xml]
+      FcrepoAdmin.associated_objects_sort_param = "#{DulHydra::IndexFields::TITLE} asc"
     end
   end
 end
