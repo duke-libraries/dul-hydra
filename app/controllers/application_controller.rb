0898f2de4918272aa443097e7e5fb6592b624b9d
Jim Coble <jim.coble@duke.edu>
2013-05-31 11:23:16 -0400

    redirect to Shib logout page after application logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,12 @@ class ApplicationController < ActionController::Base
   layout 'blacklight'
 
   protect_from_forgery
-
+  
+  private
+  
+  # Cf. https://github.com/plataformatec/devise/wiki/How-To:-Change-the-redirect-path-after-destroying-a-session-i.e.-signing-out
+  def after_sign_out_path_for(resource_or_scope)
+    "/Shibboleth.sso/Logout?return=https://shib.oit.duke.edu/cgi-bin/logout.pl"
+  end
+  
 end
