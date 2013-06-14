8db7560b6df0b23de113caea0a6246e555948aaa
dchandekstark <dchandekstark@gmail.com>
2013-06-13 20:23:20 -0400

    Removed #object_children_nav_item method b/c unused. Fixes #201.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,16 +8,4 @@ module ApplicationHelper
     !@object.is_a?(PreservationEvent) && @object.is_a?(DulHydra::Models::HasPreservationEvents)
   end
 
-  def object_children_nav_item
-    if @object.is_a?(DulHydra::Models::HasContentMetadata) && @object.datastreams[DulHydra::Datastreams::CONTENT_METADATA].has_content?
-      content_tag :li do 
-        link_to_unless_current "Children", children_path(@object)
-      end
-    elsif @object.reflections.has_key?(:children)
-      content_tag :li do
-        link_to_unless_current "Children", fcrepo_admin.object_association_path(@object, 'children')
-      end
-    end
-  end
-
 end
