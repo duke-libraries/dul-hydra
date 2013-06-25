2eaffb99c4b8e71195031f38aa44d56e0910273b
dchandekstark <dchandekstark@gmail.com>
2013-06-25 12:56:59 -0400

    removed children sort fields config

--- lib/dul_hydra.rb
+++ lib/dul_hydra.rb
@@ -1,9 +1,3 @@
 module DulHydra
-  autoload :Error, 'dul_hydra/error'
-
-  mattr_accessor :children_sort_fields
-  self.children_sort_fields = {
-    "#{DulHydra::IndexFields::IDENTIFIER} asc" => "Identifier",
-    "#{DulHydra::IndexFields::TITLE} asc" => "Title"
-  }
+  autoload :Error, 'dul_hydra/error'  
 end
