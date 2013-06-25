2cd7e823e9c04b535f9927e3891b69c3aede0ac4
dchandekstark <dchandekstark@gmail.com>
2013-06-25 11:29:01 -0400

    added .children_sort_fields config setting

--- lib/dul_hydra.rb
+++ lib/dul_hydra.rb
@@ -1,3 +1,9 @@
 module DulHydra
   autoload :Error, 'dul_hydra/error'
+
+  mattr_accessor :children_sort_fields
+  self.children_sort_fields = {
+    "#{DulHydra::IndexFields::IDENTIFIER} asc" => "Identifier",
+    "#{DulHydra::IndexFields::TITLE} asc" => "Title"
+  }
 end
