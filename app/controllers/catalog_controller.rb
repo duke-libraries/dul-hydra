80d3f9447f810d90c11d54ab8f80a12d13444475
dchandekstark <dchandekstark@gmail.com>
2013-06-24 23:04:28 -0400

    Included new module to CatalogController

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,6 +4,7 @@ require 'blacklight/catalog'
 class CatalogController < ApplicationController  
 
   include Blacklight::Catalog
+  include DulHydra::Controller::CatalogControllerBehavior
 
   # Adds method from Blacklight::SolrHelper to helper context
   helper_method :get_solr_response_for_doc_id
