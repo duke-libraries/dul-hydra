08267660fab366522769714372f7309975189071
dchandekstark <dchandekstark@gmail.com>
2013-07-18 16:00:00 -0500

    Added custom :discover ability to local Ability class, mimicking the way Hydra::Ability and Hydra::PolicyAwareAbility works. ThumnailController#can_download? customized to require :discover permission on asset. Tests added for direct and inherited object permissions.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,91 @@ class Ability
   include Hydra::PolicyAwareAbility
   include FcrepoAdmin::Ability
 
+  # Hydra::Ability provides this hook into its initialization
   def custom_permissions
+    discover_permissions
     alias_action :preservation_events, :to => :read
+  end
+
+  # Mimics Hydra::Ability#read_permissions
+  def discover_permissions
+    can :discover, String do |pid|
+      test_discover(pid)
+    end
+
+    can :discover, ActiveFedora::Base do |obj|
+      test_discover(obj.pid)
+    end 
+      
+    can :discover, SolrDocument do |obj|
+      cache.put(obj.id, obj)
+      test_discover(obj.id)
+    end 
+  end
+
+  # Mimics Hydra::Ability#test_read + Hydra::PolicyAwareAbility#test_read in one method
+  def test_discover(pid)
+    logger.debug("[CANCAN] Checking discover permissions for user: #{current_user.user_key} with groups: #{user_groups.inspect}")
+    group_intersection = user_groups & discover_groups(pid)
+    result = !group_intersection.empty? || discover_persons(pid).include?(current_user.user_key)
+    result || test_discover_from_policy(pid)
+  end 
+
+  # Mimics Hydra::PolicyAwareAbility#test_read_from_policy
+  def test_discover_from_policy(object_pid)
+    policy_pid = policy_pid_for(object_pid)
+    if policy_pid.nil?
+      return false
+    else
+      logger.debug("[CANCAN] -policy- Does the POLICY #{policy_pid} provide DISCOVER permissions for #{current_user.user_key}?")
+      group_intersection = user_groups & discover_groups_from_policy(policy_pid)
+      result = !group_intersection.empty? || discover_persons_from_policy(policy_pid).include?(current_user.user_key)
+      logger.debug("[CANCAN] -policy- decision: #{result}")
+      result
+    end
+  end 
+
+  # Mimics Hydra::Ability#read_groups
+  def discover_groups(pid)
+    doc = permissions_doc(pid)
+    return [] if doc.nil?
+    dg = edit_groups(pid) | read_groups(pid) | (doc[self.class.discover_group_field] || [])
+    logger.debug("[CANCAN] discover_groups: #{dg.inspect}")
+    return dg
+  end
+
+  # Mimics Hydra::PolicyAwareAbility#read_groups_from_policy
+  def discover_groups_from_policy(policy_pid)
+    policy_permissions = policy_permissions_doc(policy_pid)
+    discover_group_field = Hydra.config[:permissions][:inheritable][:discover][:group]
+    dg = edit_groups_from_policy(policy_pid) | read_groups_from_policy(policy_pid) | ((policy_permissions == nil || policy_permissions.fetch(discover_group_field, nil) == nil) ? [] : policy_permissions.fetch(discover_group_field, nil))
+    logger.debug("[CANCAN] -policy- discover_groups: #{dg.inspect}")
+    return dg
+  end
+
+  # Mimics Hydra::Ability#read_persons
+  def discover_persons(pid)
+    doc = permissions_doc(pid)
+    return [] if doc.nil?
+    dp = edit_persons(pid) | read_persons(pid) | (doc[self.class.discover_person_field] || [])
+    logger.debug("[CANCAN] discover_persons: #{dp.inspect}")
+    return dp
+  end
+
+  def discover_persons_from_policy(policy_pid)
+    policy_permissions = policy_permissions_doc(policy_pid)
+    discover_individual_field = Hydra.config[:permissions][:inheritable][:discover][:individual]
+    dp = edit_persons_from_policy(policy_pid) | read_persons_from_policy(policy_pid) | ((policy_permissions == nil || policy_permissions.fetch(discover_individual_field, nil) == nil) ? [] : policy_permissions.fetch(discover_individual_field, nil))
+    logger.debug("[CANCAN] -policy- discover_persons: #{dp.inspect}")
+    return dp
+  end
+
+  def self.discover_person_field 
+    Hydra.config[:permissions][:discover][:individual]
+  end
+
+  def self.discover_group_field
+    Hydra.config[:permissions][:discover][:group]
   end
 
 end
---
+++ spec/controllers/thumbnail_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe ThumbnailController do
+  let(:object) { FactoryGirl.create(:component_with_content) }
+  after { object.delete }
+  context "user with discover permssion but not read permission on asset" do
+    before do
+      object.read_groups = ["registered"]
+      object.discover_groups = ["public"]
+      object.save
+    end
+    it "should allow user to download thumbnail" do
+      get :show, :id => object
+      response.should be_successful
+    end
+  end
+  context "user with discover policy permission, but not read permission, on asset" do
+    let(:policy) { AdminPolicy.create }
+    before do
+      policy.default_permissions = [{type: 'group', name: 'public', access: 'discover'},
+                                    {type: 'group', name: 'registered', access: 'read'}]
+      policy.save
+      object.admin_policy = policy
+      object.save
+    end
+    after { policy.delete }
+    it "should allow user to download thumbnail" do
+      get :show, :id => object
+      response.should be_successful
+    end
+  end
+end
