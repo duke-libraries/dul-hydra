c01a9e38b3329fe429c04990dd1b77f15513b59e
dchandekstark <dchandekstark@gmail.com>
2013-07-22 14:36:10 -0500

    Changed #render_thumbanil helper to use thumbnail_path. Updated integration test.

--- spec/features/catalog/index.html.erb_spec.rb
+++ spec/features/catalog/index.html.erb_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe "catalog/index.html.erb" do
   subject { page }
-  let(:object) { FactoryGirl.create(:test_content_thumbnail) }
+  let(:object) { FactoryGirl.create(:component_with_content) }
+  before do
+    object.discover_groups = ["public"]
+    object.save
+  end
   after { object.delete }
   it "should display thumbnails" do
     visit catalog_index_path
     fill_in "q", :with => object.title.first
     click_button "search"
-    expect(subject).to have_xpath("//a[@href = \"#{catalog_path(object)}\"]/img[@src = \"#{fcrepo_admin.download_object_datastream_path(object, DulHydra::Datastreams::THUMBNAIL)}\"]")
+    expect(subject).to have_xpath("//a[@href = \"#{catalog_path(object)}\"]/img[@src = \"#{thumbnail_path(object)}\"]")
   end
 end
