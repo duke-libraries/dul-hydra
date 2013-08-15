91f4ce658e1fb46673bb3160d009378d88178741
Jim Coble <jim.coble@duke.edu>
2013-08-15 15:25:41 -0400

    work in progress on web interface

--- lib/dul_hydra/batch/jobs/batch_processor_job.rb
+++ lib/dul_hydra/batch/jobs/batch_processor_job.rb
@@ -1,7 +1,9 @@
 module DulHydra::Batch::Jobs
   class BatchProcessorJob < Struct.new(:batch_id)
     def perform
-      bp = DulHydra::Batch::Scripts::BatchProcessor.new(:batch_id => batch_id)
+      ts = Time.now.to_i
+      logfile = "batch_processor_#{ts}.log"
+      bp = DulHydra::Batch::Scripts::BatchProcessor.new(:batch_id => batch_id, :log_file => logfile)
       bp.execute
     end
   end
