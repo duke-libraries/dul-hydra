09547ed4b2ad6eba265861a3bf7c8143919ff42d
dchandekstark <dchandekstark@gmail.com>
2013-09-25 14:50:33 -0400

    Security fix

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DulHydra::Application.config.secret_token = 'c321ad5e19393993af0ea99eb4021b1fb75fc6f950e627122aa7812986e5c03a38a24e293e371240eca727ffba53648241b26885d6c4eb0d6cb9227fd2feea6a'
