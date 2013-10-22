595e924405bdc9f47e5d734107ddcec0262a3f65
dchandekstark <dchandekstark@gmail.com>
2013-10-22 13:44:18 -0400

    Removed override of #escape_filter, which has been incorporated into hydra-head 6.4 (Fixzes #318).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,4 @@ class ApplicationController < ActionController::Base
   include Hydra::PolicyAwareAccessControlsEnforcement
   include DulHydra::Controller::ControllerBehavior
 
-  # Patches Hydra::AccessControlsEnforcement#escape_filter to support escaping colons
-  # cf. https://github.com/projecthydra/hydra-head/pull/115
-  def escape_filter(key, value)
-    [key, value.gsub(/[ :\/]/, ' ' => '\ ', '/' => '\/', ':' => '\:')].join(':')
-  end
-
 end
