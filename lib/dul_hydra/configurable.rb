3add02488f4776fe16d033ea449fbc003830288d
dchandekstark <dchandekstark@gmail.com>
2013-10-24 09:35:26 -0400

    Fixes #296

--- lib/dul_hydra/configurable.rb
+++ lib/dul_hydra/configurable.rb
@@ -2,10 +2,6 @@ module DulHydra::Configurable
   extend ActiveSupport::Concern
 
   included do
-    # Ordering of descriptive metadata fields on object page
-    mattr_accessor :metadata_fields
-    self.metadata_fields = [:title, :identifier, :source, :description, :date, :creator, :contributor, :publisher, :language, :subject, :type, :relation, :coverage, :rights]
-
     mattr_accessor :unwanted_models
 
     mattr_accessor :export_set_manifest_file_name
