8098232daa3cb81c7bbc4fcb35384f4cecdda29c
dchandekstark <dchandekstark@gmail.com>
2013-10-29 17:54:11 -0400

    Fixes #348

--- lib/dul_hydra/models/user.rb
+++ lib/dul_hydra/models/user.rb
@@ -21,7 +21,7 @@ module DulHydra::Models
     # user class to get a user-displayable login/identifier for
     # the account. 
     def to_s
-      username
+      user_key
     end
 
     def ability
