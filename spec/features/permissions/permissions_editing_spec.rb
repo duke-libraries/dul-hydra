2c7baf0c0fc64061ff8f835ab45d51d44170612d
dchandekstark <dchandekstark@gmail.com>
2013-11-14 14:41:37 -0500

    Added integration tests for permissions editing - Fixes #377. Tweaked permissions edit view layout.

---
+++ spec/features/permissions/permissions_editing_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "permissions editing" do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:object) { FactoryGirl.create(:test_model) }
+  before do
+    object.edit_users = [user.user_key]
+    object.read_groups = ["registered"]
+    object.discover_groups = ["public"]
+    object.save
+    login_as user
+  end
+  after do
+    Warden.test_reset!
+  end
+  it "should be idempotent" do
+    original_permissions = object.permissions
+    visit permissions_edit_path(object)
+    click_button "Save"
+    object.reload
+    object.permissions.should == original_permissions
+    object.edit_users.should == [user.user_key]
+    object.read_groups.should == ["registered"]
+    object.discover_groups.should == ["public"]
+  end
+  it "should be able to remove a permission" do
+    visit permissions_edit_path(object)
+    page.unselect "Public", from: "permissions_discover_groups"
+    click_button "Save"
+    object.reload
+    object.discover_groups.should be_empty
+  end
+  it "should be able to add a permission" do
+    visit permissions_edit_path(object)
+    page.select "Duke Community", from: "permissions_edit_groups"
+    click_button "Save"
+    object.reload
+    object.edit_groups.should == ["registered"]
+  end
+end
