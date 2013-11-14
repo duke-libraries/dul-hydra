b5ff37b2e5a44e7196e2e4ae2b124fe0e80d80bd
Jim Coble <jim.coble@duke.edu>
2013-11-14 16:34:20 -0500

    account for pre-assigned PIDs in batch object relationship validation; closes #381

--- lib/dul_hydra/batch/models/batch.rb
+++ lib/dul_hydra/batch/models/batch.rb
@@ -3,7 +3,7 @@ module DulHydra::Batch::Models
   class Batch < ActiveRecord::Base
     attr_accessible :description, :name, :user, :details, :failure, :logfile, :outcome, :start, :status, :stop, :success, :total, :version
     belongs_to :user, :inverse_of => :batches
-    has_many :batch_objects, :inverse_of => :batch, :dependent => :destroy
+    has_many :batch_objects, :inverse_of => :batch, :dependent => :destroy, :order => 'id ASC'
     has_attached_file :logfile
 
     OUTCOME_SUCCESS = "SUCCESS"
@@ -17,6 +17,14 @@ module DulHydra::Batch::Models
       errors = []
       batch_objects.each { |object| errors << object.validate }
       errors.flatten
+    end
+    
+    def pre_assigned_pids
+      @pre_assigned_pids ||= collect_pre_assigned_pids
+    end
+    
+    def collect_pre_assigned_pids
+      batch_objects.map{ |x| x.pid if x.pid.present? }.compact      
     end
     
   end
