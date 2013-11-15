4a47ce5bf09631656f6853a12c2df8d50d2949f4
Jim Coble <jim.coble@duke.edu>
2013-11-15 14:38:00 -0500

    add validations

--- app/controllers/ingest_folders_controller.rb
+++ app/controllers/ingest_folders_controller.rb
@@ -12,12 +12,21 @@ class IngestFoldersController < ApplicationController
   end
   
   def create
+    @admin_policies = AdminPolicy.all
+    collections = Collection.all
+    collection_hash = {}
+    collections.each { |coll| collection_hash[coll.title.first] = coll.pid }
+    @collection_options = Hash[collection_hash.sort]
+    @permitted_folder_bases = IngestFolder.permitted_folders(current_user)
     @ingest_folder = IngestFolder.new(params[:ingest_folder])
     @ingest_folder.model = IngestFolder.default_file_model
     @ingest_folder.add_parents = true
     @ingest_folder.user = current_user
-    @ingest_folder.save
-    redirect_to :action => :show, :id => @ingest_folder
+    if @ingest_folder.save
+      redirect_to :action => :show, :id => @ingest_folder
+    else
+      render :new
+    end
   end
   
   def show
