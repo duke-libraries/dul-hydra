04dae65788d7a2266ac19318936dd28c5a9cdef2
dchandekstark <dchandekstark@gmail.com>
2013-11-26 18:06:25 -0500

    Refactoring done to push datastream file name methods to ActiveFedora::Datastream. DownloadsController#datastream_name should now adhere more closely to our expectations. Fixed #386.

--- spec/controllers/downloads_controller_spec.rb
+++ spec/controllers/downloads_controller_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe DownloadsController do
   let(:user) { FactoryGirl.create(:user) }
-  let(:obj) { FactoryGirl.create(:test_content) }
   after do
     user.delete
     obj.delete
@@ -12,8 +11,25 @@ describe DownloadsController do
     obj.save
     sign_in user
   end
-  it "should download successfully" do
-    get :show, id: obj
-    response.should be_successful
+  context "object (i.e. content) download" do
+    let(:obj) { FactoryGirl.create(:test_content) }
+    it "should download successfully" do
+      get :show, id: obj
+      response.should be_successful
+    end
+  end
+  context "descMetadata download" do
+    let(:obj) { FactoryGirl.create(:collection) }
+    it "should download successfully" do
+      get :show, id: obj, datastream_id: "descMetadata"
+      response.should be_successful
+    end
+  end
+  context "rightsMetadata download" do
+    let(:obj) { FactoryGirl.create(:admin_policy) }
+    it "should download successfully" do
+      get :show, id: obj, datastream_id: "rightsMetadata"
+      response.should be_successful
+    end
   end
 end
