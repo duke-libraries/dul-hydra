653da05bea40b66b3901c586be6bf42f29bc2870
dchandekstark <dchandekstark@gmail.com>
2013-12-04 23:37:26 -0500

    Load environment variables from config/local_env.yml. Secret token initializer gets value from environment on production, uses generated value on development and test.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DulHydra::Application.config.secret_token = 'c321ad5e19393993af0ea99eb4021b1fb75fc6f950e627122aa7812986e5c03a38a24e293e371240eca727ffba53648241b26885d6c4eb0d6cb9227fd2feea6a'
+DulHydra::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+                                              SecureRandom.hex(64)
+                                            else
+                                              ENV['SECRET_TOKEN']
+                                            end
