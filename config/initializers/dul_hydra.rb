29cf94948e663abc4d3b8238ff81260eaf986501
dchandekstark <dchandekstark@gmail.com>
2014-01-14 11:58:28 -0500

    Fixes #494

--- config/initializers/dul_hydra.rb
+++ config/initializers/dul_hydra.rb
@@ -23,6 +23,14 @@ DulHydra.configure do |config|
   config.creatable_models = ["AdminPolicy", "Collection"]
 
   config.superuser_group = ENV['SUPERUSER_GROUP']
+
+  config.csv_options = { 
+    encoding: "UTF-8",
+    col_sep: "\t",
+    headers: true,
+    write_headers: true,
+    header_converters: :symbol
+  }
 end
 
 # Load configuration for Grouper service, if present
