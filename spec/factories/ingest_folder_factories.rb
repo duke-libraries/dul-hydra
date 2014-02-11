4b8ac9dc5022611f622342a4728297aac9157dee
Jim Coble <jim.coble@duke.edu>
2014-02-11 16:42:05 -0500

    ingest folder batch object access controls change; closes #605

--- spec/factories/ingest_folder_factories.rb
+++ spec/factories/ingest_folder_factories.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     model "TestChild"
     base_path "base/path/"
     sub_path "subpath"
-    admin_policy_pid "apo:123"
     collection_pid "test:456"
     add_parents true
     parent_id_length 1
