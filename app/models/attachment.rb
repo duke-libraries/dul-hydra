6549325b6711316d3ee458c8911c55bb61839e1d
Jim Coble <jim.coble@duke.edu>
2014-02-12 10:21:23 -0500

    attachment initial permissions now conform to initial object permissions policy; closes #606

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -14,10 +14,13 @@ class Attachment < DulHydra::Base
 
   def set_initial_permissions(user_creator = nil)
     if attached_to
-      self.permissions_attributes = attached_to.permissions.collect { |p| p.vals }
-      # XXX In active-fedora 7.0 can do
-      # self.admin_policy = attached_to.admin_policy
-      self.admin_policy_id = attached_to.admin_policy_id if attached_to.has_admin_policy?
+      if attached_to.has_admin_policy?
+        # XXX In active-fedora 7.0 can do
+        # self.admin_policy = attached_to.admin_policy
+        self.admin_policy_id = attached_to.admin_policy_id if attached_to.has_admin_policy?
+      else      
+        self.permissions_attributes = attached_to.permissions.collect { |p| p.vals }
+      end
     end
     if user_creator
       self.permissions_attributes = [{type: 'user', name: user_creator.user_key, access: 'edit'}]
