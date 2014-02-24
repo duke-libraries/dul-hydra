c21b64d29a2926f1b803efcfa8878c826363cc28
Jim Coble <jim.coble@duke.edu>
2014-02-24 13:00:44 -0500

    refactor location of modules in file so can be used in validations; closes #655

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -2,28 +2,6 @@ class EventLog < ActiveRecord::Base
   
   belongs_to :user, inverse_of: :event_logs
   
-  validates_presence_of :event_date_time, :agent_type, :action, :model, :object_identifier, :application_version
-  validate :agent_type_must_be_valid
-  validate :action_must_be_valid
-  validates_presence_of :user, if: :person_event?
-  validates_presence_of :software_agent_value, if: :software_event?
-  
-  def self.agent_types
-    AgentTypes.values
-  end
-  
-  def self.actions
-    Actions.values
-  end
-  
-  def person_event?
-    agent_type == AgentTypes::PERSON
-  end
-  
-  def software_event?
-    agent_type == AgentTypes::SOFTWARE
-  end
-
   module AgentTypes
     PERSON = "person"
     SOFTWARE = "software"
@@ -48,18 +26,18 @@ class EventLog < ActiveRecord::Base
     end
   end
   
-  private
+  validates_presence_of :event_date_time, :agent_type, :action, :model, :object_identifier, :application_version
+  validates_inclusion_of :agent_type, in: AgentTypes::values
+  validates_inclusion_of :action, in: Actions::values
+  validates_presence_of :user, if: :person_event?
+  validates_presence_of :software_agent_value, if: :software_event?
   
-  def agent_type_must_be_valid
-    unless EventLog.agent_types.include?(agent_type)
-      errors.add(:agent_type, I18n.t('dul_hydra.event_logs.alerts.agent_type.invalid') % agent_type)
-    end
+  def person_event?
+    agent_type == AgentTypes::PERSON
+  end
+  
+  def software_event?
+    agent_type == AgentTypes::SOFTWARE
   end
 
-  def action_must_be_valid
-    unless EventLog.actions.include?(action)
-      errors.add(:action, I18n.t('dul_hydra.event_logs.alerts.action.invalid') % action)
-    end
-  end
-  
 end
