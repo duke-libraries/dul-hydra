1b55266625aa612406cbe8cc498d37841c02064b
dchandekstark <dchandekstark@gmail.com>
2014-03-19 18:05:05 -0400

    WIP [ci skip]

--- spec/scripts/batch_fixity_check_spec.rb
+++ spec/scripts/batch_fixity_check_spec.rb
@@ -3,63 +3,48 @@ require 'spec_helper'
 describe "BatchFixityCheck", fixity_check: true do
   let(:report) { Tempfile.new('batch_fixity_check_spec') }
   let!(:bfc) { DulHydra::Scripts::BatchFixityCheck.new(report: report.path) }
-  before { ActiveFedora::Base.destroy_all }
   after do
     report.close!
-    #ActiveFedora::Base.destroy_all
+    ActiveFedora::Base.destroy_all
   end
-  describe "the process", fixme: true do
-    before do
-      @obj1 = FactoryGirl.create(:component)
-      @obj2 = FactoryGirl.create(:item)
-      @obj3 = FactoryGirl.create(:collection)
-      @obj4 = FactoryGirl.create(:target)
-      @obj5 = FactoryGirl.create(:admin_policy)
-
-      @obj1.fixity_check! # last fixity check is now
-      @obj2.fixity_check! # last fixity check is now
-      fc = @obj3.fixity_check # last fixity check one year ago
-      fc.event_date_time = PreservationEvent.to_event_date_time(Time.now.ago(1.year).utc)
-      fc.save
-      bfc.execute
-    end
-    it "should run fixity checks on objects" do
-      @obj1.fixity_checks.to_a.size.should == 1
-      @obj2.fixity_checks.to_a.size.should == 1
-      @obj3.fixity_checks.to_a.size.should == 2
-      @obj4.fixity_checks.to_a.size.should == 1
-      PreservationEvent.events_for(@obj5.pid).count.should == 0
-      bfc.total.should == 2
-      bfc.outcome_counts[PreservationEvent::SUCCESS].should == 2
-    end
+  before do
+    @obj1 = FactoryGirl.create(:component)
+    @obj2 = FactoryGirl.create(:item)
+    @obj3 = FactoryGirl.create(:collection)
+    @obj4 = FactoryGirl.create(:target)
+    @obj5 = FactoryGirl.create(:admin_policy)
+    
+    @obj1.fixity_check! # last fixity check is now
+    @obj2.fixity_check! # last fixity check is now
+    fc = @obj3.fixity_check # last fixity check one year ago
+    fc.event_date_time = PreservationEvent.to_event_date_time(Time.now.ago(1.year).utc)
+    fc.save
+    bfc.execute
+  end
+  it "should run fixity checks on objects" do
+    expect(@obj1.fixity_checks.count).to eq(1)
+    expect(@obj2.fixity_checks.count).to eq(1)
+    expect(@obj3.fixity_checks.count).to eq(2)
+    expect(@obj4.fixity_checks.count).to eq(1)
+    expect(PreservationEvent.events_for(@obj5.pid).count).to eq(0)
+    expect(bfc.total).to eq(2)
+    expect(bfc.outcome_counts[PreservationEvent::SUCCESS]).to eq(2)
   end
   describe "the report" do
-    let(:obj) { FactoryGirl.create(:component_with_content) }
-    let!(:report_map) { {} }
-    before do
-      fc = obj.fixity_check # last fixity check one year ago
-      fc.event_date_time = PreservationEvent.to_event_date_time(Time.now.ago(1.year).utc)
-      fc.save
-      bfc.execute
-      # XXX This is brittle, but so is the report
-      CSV.foreach(report.path) do |row|
-        pid = row[0]
-        report_map[pid] ||= []
-        report_map[pid] << row[1]
-      end
-    end
-    after do
-      obj.destroy
+    let(:obj) { @obj3 }
+    let(:csv) { CSV.read(report.path, headers: true) }
+    it "should have a header row" do
+      expect(csv.headers).to eq(['PID', 'Datastream', 'dsVersionID', 'dsCreateDate', 'dsChecksumType', 'dsChecksum', 'dsChecksumValid'])
     end
     it "should have rows for all datastreams that have content" do
-      obj.datastreams.select {|dsid, ds| ds.has_content?}.each do |dsid, ds|
-        report_map[obj.pid].should include(dsid)
-      end
+      # expect(obj.datastreams["DC"]).to have_content
+      # XXX A bug in ActiveFedora or Rubydora causes the DC datastream to appear initially empty
+      expect(obj.datastreams.select {|dsid, ds| ds.has_content?}.keys)
+        .to match_array(csv.select {|row| row["PID"] == obj.pid}.collect {|row| row["Datastream"]})
     end
     it "should not have rows for datastreams have do not have content" do
-      obj.datastreams.reject {|dsid, ds| ds.has_content?}.each do |dsid, ds|
-        report_map[obj.pid].should_not include(dsid)
-      end
+      expect(csv.select {|row| row["PID"] == obj.pid}.collect {|row| row["Datastream"]})
+        .not_to include(obj.datastreams.reject {|dsid, ds| ds.has_content?})
     end
   end
 end
