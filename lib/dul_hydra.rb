a449a8706037e46421009735453124afd4d41731
dchandekstark <dchandekstark@gmail.com>
2014-11-07 14:11:24 -0500

    Removed dead autloads of exception classes (closes #1202)

--- lib/dul_hydra.rb
+++ lib/dul_hydra.rb
@@ -4,10 +4,6 @@ module DulHydra
 
   autoload :Configurable, 'dul_hydra/configurable'
 
-  autoload :Error, 'dul_hydra/error'
-  autoload :ChecksumInvalid, 'dul_hydra/error'
-  autoload :VirusFoundError, 'dul_hydra/error'
-
   include DulHydra::Configurable
 
 end
