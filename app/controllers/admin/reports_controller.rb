a63c428a6e95477ec485077965fca05fee77b07b
David Chandek-Stark <dchandekstark@gmail.com>
2015-08-11 11:49:16 -0400

    Removes extra .csv file extension from repository report
    Fixes #1405

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -5,14 +5,13 @@ module Admin
 
     def show
       report = DulHydra::Reports::Report.new
-      basename = "Duke_Digital_Repository"
+      filename = "Duke_Digital_Repository"
       case params.require(:type)
       when "techmd"
         report.filters << DulHydra::Reports::HasContentFilter
         report.columns += DulHydra::Reports::Columns::TechnicalMetadata
-        basename += "__TECHMD"
+        filename += "__TECHMD"
       end
-      filename = basename + ".csv"
       render csv: report, filename: filename
     end
 
