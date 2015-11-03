69444870c9d079ccccd7ae4e6af73f143ea969d8
Jim Coble <jim.coble@duke.edu>
2015-11-03 17:30:07 -0500

    fix failing descriptive metadata table tests

--- spec/lib/descriptive_metadata_table_spec.rb
+++ spec/lib/descriptive_metadata_table_spec.rb
@@ -11,7 +11,13 @@ module DulHydra
       dm_table = described_class.new(objects)
       objects.each_with_index do |obj, i|
         dm_table.headers.each do |h|
-          value = obj.send(h)
+          value = case h
+                    when :pid
+                      obj.pid
+                    else
+                      obj.descMetadata.send(h)
+                  end
+          # value = obj.descMetadata.send(h)
           value = value.first if value.is_a? Array
           expect(dm_table[i][h]).to eq(value)
         end
@@ -19,9 +25,9 @@ module DulHydra
     end
     context "repeated columns" do
       before do
-        objects[0].identifier = ["foo", "bar", "baz"]
+        objects[0].dc_identifier = ["foo", "bar", "baz"]
         objects[0].save
-        objects[1].identifier = ["spam", "eggs"]
+        objects[1].dc_identifier = ["spam", "eggs"]
         objects[1].save
       end
       it "should have repeated headers" do
@@ -33,10 +39,10 @@ module DulHydra
         identifier_indices = first_identifier_index .. first_identifier_index+2
         objects.each_with_index do |obj, i|
           expect(dm_table[i][:pid]).to eq(obj.id)
-          expect(dm_table[i][:title]).to eq(obj.title.first)
+          expect(dm_table[i][:title]).to eq(obj.dc_title.first)
           j = 0
           while idx = dm_table[i].index(:identifier, first_identifier_index + j)
-            expect(dm_table[i][idx]).to eq(obj.identifier[j])
+            expect(dm_table[i][idx]).to eq(obj.dc_identifier[j])
             j += 1
           end
         end
