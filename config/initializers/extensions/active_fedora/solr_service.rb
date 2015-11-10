b4b46405ee87f5278f589dcaa170cb00a80e83cf
David Chandek-Stark <david.chandek.stark@duke.edu>
2015-11-10 17:16:40 -0500

    Update solr_service.rb
    Proposing a slight rewrite.

--- config/initializers/extensions/active_fedora/solr_service.rb
+++ config/initializers/extensions/active_fedora/solr_service.rb
@@ -1,18 +1,11 @@
-require 'rsolr'
-
-module ActiveFedora
-  class SolrService
-    extend Deprecation
-
-    class << self
-      def post_query(query, args={})
-        raw = args.delete(:raw)
-        args = args.merge(:q=>query, :qt=>'standard')
-        result = SolrService.instance.conn.post('select', :data=>args)
-        return result if raw
-        result['response']['docs']
-      end
-    end
-
+ActiveFedora::SolrService.class_eval do
+  
+  def self.post_query(query, args={})
+    raw = args.delete(:raw)
+    args = args.merge(:q=>query, :qt=>'standard')
+    result = instance.conn.post('select', :data=>args)
+    return result if raw
+    result['response']['docs']
   end
+
 end
