duke-libraries/dul-hydra

commit 94b87743aa5395bdac68e75d0c89d53e311ed86f

Add COLLECTION_ID argument to rake task for simple ingest; closes #1683.
Also, minor refactoring of other rake task arguments.

lib/tasks/dul_hydra.rake
@@ -48,14 +48,15 @@ namespace :dul_hydra do
   end
 
   namespace :batch do
-    desc "Creates ingest batch from Simple Ingest Format directory: *FOLDER, *BATCH_USER, ADMIN_SET, CONFIG_FILE"
+    desc "Creates ingest batch from Simple Ingest Format directory: *FOLDER, *BATCH_USER, ADMIN_SET, COLLECTION_ID, CONFIG_FILE"
     task :simple_ingest => :environment do
       raise "Must specify folder path. Ex.: FOLDER=/path/to/simple/ingest/folder" unless ENV['FOLDER']
       raise "Must specify batch user.  Ex.: BATCH_USER=tom@school.edu" unless ENV['BATCH_USER']
       processor_args = { filepath: ENV['FOLDER'] }
-      processor_args[:config_file] = ENV['CONFIG_FILE'] if ENV['CONFIG_FILE']
       processor_args[:batch_user] = ENV['BATCH_USER']
-      processor_args[:admin_set] = ENV['ADMIN_SET'] if ENV['ADMIN_SET']
+      processor_args[:admin_set] = ENV['ADMIN_SET']
+      processor_args[:collection_id] = ENV['COLLECTION_ID']
+      processor_args[:config_file] = ENV['CONFIG_FILE']
       processor = DulHydra::Scripts::ProcessSimpleIngest.new(processor_args)
       processor.execute
     end

commit d8538ded2be8773b12b2940580feb2fe33fd5eeb

Fix config file bug in simple ingest rake task / application script.
Bug introduced with commit 94b87743aa5395bdac68e75d0c89d53e311ed86f.

lib/dul_hydra/version.rb
@@ -1,3 +1,3 @@
 module DulHydra
-  VERSION = "5.0.1"
+  VERSION = "5.0.2"
 end

lib/tasks/dul_hydra.rake
@@ -56,7 +56,7 @@ namespace :dul_hydra do
       processor_args[:batch_user] = ENV['BATCH_USER']
       processor_args[:admin_set] = ENV['ADMIN_SET']
       processor_args[:collection_id] = ENV['COLLECTION_ID']
-      processor_args[:config_file] = ENV['CONFIG_FILE']
+      processor_args[:config_file] = ENV['CONFIG_FILE'] if ENV['CONFIG_FILE']
       processor = DulHydra::Scripts::ProcessSimpleIngest.new(processor_args)
       processor.execute
     end