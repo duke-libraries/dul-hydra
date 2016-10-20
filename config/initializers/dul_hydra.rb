ffffe5958102261ec72b165f857d1925a0ea5f1a
David Chandek-Stark <dchandekstark@gmail.com>
2016-10-20 16:26:24 -0400

    Adds icc_profile_name and icc_profile_version to technical metadata show view.
    Changes in ddr-models automatically add those fields to the technical metadata
    report for a collection, which completes DDR-86.

--- config/initializers/dul_hydra.rb
+++ config/initializers/dul_hydra.rb
@@ -1,7 +1,6 @@
 require 'dul_hydra'
 
 DulHydra.configure do |config|
-  config.collection_report_fields = [:pid, :identifier, :content_size, :content_checksum]
   config.contact_email = ENV['CONTACT_EMAIL']
   config.help_url = Rails.env.test? ? "http://www.loc.gov" : ENV['HELP_URL']
   config.csv_options = {
