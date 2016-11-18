eee56e26cc45f7109f253b15ae625b615e6f0f45
David Chandek-Stark <david.chandek.stark@duke.edu>
2016-11-18 16:22:57 -0500

    Hotfix 4.4.5 (#1794)
    Adds initializer for AS::Notifications subscriptions.
    Moves ReindexCollectionContents subscription to initializer.

--- app/services/reindex_collection_contents.rb
+++ app/services/reindex_collection_contents.rb
@@ -37,8 +37,3 @@ class ReindexCollectionContents
   end
 
 end
-
-ActiveSupport::Notifications.subscribe("save.collection") do |*args|
-  event = ActiveSupport::Notifications::Event.new(*args)
-  ReindexCollectionContents.trigger(event)
-end
--- lib/dul_hydra/version.rb
+++ lib/dul_hydra/version.rb
@@ -1,3 +1,3 @@
 module DulHydra
-  VERSION = "4.4.4"
+  VERSION = "4.4.5"
 end
