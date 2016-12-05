86d11068721f906453685a605131251a7df8ce78
David Chandek-Stark <david.chandek.stark@duke.edu>
2016-12-05 11:59:02 -0500

    Update batches_helper.rb

--- app/helpers/batches_helper.rb
+++ app/helpers/batches_helper.rb
@@ -81,8 +81,7 @@ module BatchesHelper
     end
 
     def render_batch_delete_link(batch)
-      case batch.status
-      when nil, Ddr::Batch::Batch::STATUS_READY, Ddr::Batch::Batch::STATUS_VALIDATED, Ddr::Batch::Batch::STATUS_INVALID
+      if can?(:destroy, batch) && [nil, Ddr::Batch::Batch::STATUS_READY, Ddr::Batch::Batch::STATUS_VALIDATED, Ddr::Batch::Batch::STATUS_INVALID].include?(batch.status)
         link_to content_tag(:span, "", :class => "glyphicon glyphicon-trash"), {:action => 'destroy', :id => batch}, :method => 'delete', :id => "batch_delete_#{batch.id}", :data => { :confirm => "#{t('batch.web.batch_deletion_confirmation', batch_id: batch.id)}" }
       end
     end
