cb3cae27841ec771e06a65ef0390c5ce3f45ac66
David Chandek-Stark <dchandekstark@gmail.com>
2016-12-09 17:08:34 -0500

    WIP

--- app/models/permanent_id.rb
+++ app/models/permanent_id.rb
@@ -1,10 +1,12 @@
 require 'ezid-client'
 
-class PermanentId < Ezid::Identifier
+class PermanentId
 
   class Error < DulHydra::Error; end
+  class AssignmentFailed < Error; end
   class ObjectNotPersisted < Error; end
-  class AlreadyAssigned < Error; end
+  class AlreadyAssigned < AssignmentFailed; end
+  class IdentifierNotFound < Error; end
 
   DEFAULT_STATUS   = Ezid::Status::RESERVED
   DEFAULT_EXPORT   = "no"
@@ -12,57 +14,76 @@ class PermanentId < Ezid::Identifier
   DEFAULT_TARGET   = "https://repository.duke.edu/id/%s"
   FCREPO3_PID      = "fcrepo3.pid".freeze
 
-  self.defaults = {
+  class_attribute :identifier_class
+
+  self.identifier_class = Ezid::Identifier
+  self.identifier_class.defaults = {
     profile: "dc",
     export:  "no",
     status:  DEFAULT_STATUS,
   }
 
-  # @return [PermanentId] the permanent id assigned
-  def self.assign!(obj, ark: nil)
-    if obj.new_record?
-      raise ObjectNotPersisted,
-            "Object must be persisted prior to permanent id assignment."
-    end
-    if obj.permanent_id
-      raise AlreadyAssigned,
-            "Object #{obj.id} has already been assigned permanent id \"#{obj.permanent_id}\"."
-    end
-    identifier = ark ? find(ark) : mint
-    obj.reload
-    obj.permanent_id = identifier.id
-    obj.save(validate: false)
-    identifier.set_metadata!(obj)
+  # @return [PermanentId] the permanent id assigned to the object
+  def self.assign!(obj, ark = nil)
+    new(obj).assign!(ark)
   end
 
-  # @return
+  # @return [PermanentId] the permanent id previously assigned to the object,
+  #   or nil, if not assigned.
   def self.assigned(obj)
-    find(obj.permanent_id) if obj.permanent_id
+    perm_id = new(obj)
+    perm_id.assigned? ? perm_id : nil
+  end
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def assign!(id = nil)
+    ActiveSupport::Notifications.instrument("assign.permanent_id", pid: object.id) do |payload|
+      assign(id)
+      set_metadata!
+      payload.merge(status: status, target: target, identifier: identifier.id)
+    end
+  end
+
+  def assigned?
+    object.permanent_id
+  end
+
+  def identifier
+    if @identifier.nil? && assigned?
+      @identifier = find_identifier(object.permanent_id)
+    end
+    @identifier
   end
 
   # @raise [Ezid::Error] on EZID client or server error
-  def set_metadata!(obj)
-    set_metadata(obj)
+  def set_metadata!
+    set_metadata
     save
+    self
   end
 
-  def set_metadata(obj)
+  def set_metadata
     set_target
-    set_status(obj)
-    set_repo_id(obj)
+    set_status
+    set_repo_id
   end
 
   def set_target
     self.target = DEFAULT_TARGET % id
   end
 
-  def set_repo_id(obj)
-    self.repo_id = obj.id
+  def set_repo_id
+    self.repo_id = object.id
   end
 
   def repo_id=(val)
     if repo_id
-      raise Error, "#{repo_id_field} already set to \"#{repo_id}\", cannot change."
+      raise Error, "Identifier repository id already set to \"#{repo_id}\"; cannot change."
     end
     self[repo_id_field] = val
   end
@@ -75,11 +96,58 @@ class PermanentId < Ezid::Identifier
     FCREPO3_PID
   end
 
-  def set_status(obj)
-    if obj.published? && !public?
+  def set_status!
+    save if set_status
+  end
+
+  def set_status
+    if object.published? && !public?
       public!
-    elsif obj.unpublished? && public?
+    elsif object.unpublished? && public?
       unavailable!("not published")
+    end
+  end
+
+  protected
+
+  def method_missing(name, *args, &block)
+    identifier.send(name, *args, &block)
+  end
+
+  private
+
+  def find_identifier(ark)
+    identifier_class.find(ark)
+  rescue Ezid::IdentifierNotFoundError => e
+    raise IdentifierNotFound, e.message
+  end
+
+  def mint_identifier(*args)
+    identifier_class.mint(*args)
+  end
+
+  def assign(id = nil)
+    if object.new_record?
+      raise ObjectNotPersisted,
+            "Object must be persisted prior to permanent id assignment."
+    end
+    if assigned?
+      raise AlreadyAssigned,
+            "Object \"#{object.id}\" has already been assigned permanent id \"#{object.permanent_id}\"."
+    end
+    @identifier = case id
+                  when identifier_class
+                    id
+                  when String
+                    find_identifier(id)
+                  when nil
+                    mint_identifier
+                  end
+    object.reload
+    object.permanent_id = identifier.id
+    unless object.save(validate: false)
+      raise AssignmentFailed,
+            "Assignment of permanent id \"#{identifier.id}\" to object \"#{object.id}\" failed."
     end
   end
 
--- spec/models/permanent_id_spec.rb
+++ spec/models/permanent_id_spec.rb
@@ -2,13 +2,6 @@ RSpec.describe PermanentId do
 
   let(:obj) { FactoryGirl.create(:item) }
 
-  describe "defaults" do
-    subject { described_class.new("foo") }
-    its(:profile) { is_expected.to eq("dc") }
-    its(:export) { is_expected.to eq("no") }
-    its(:status) { is_expected.to eq("reserved") }
-  end
-
   describe ".assign!" do
     describe "when the object is new" do
       let(:obj) { FactoryGirl.build(:item) }
@@ -17,6 +10,7 @@ RSpec.describe PermanentId do
       end
     end
     describe "when the object already has a permanent identifier" do
+      let(:obj) { FactoryGirl.create(:item) }
       before { obj.permanent_id = "foo" }
       it "raises an error" do
         expect { described_class.assign!(obj) }.to raise_error(PermanentId::AlreadyAssigned)
@@ -24,136 +18,148 @@ RSpec.describe PermanentId do
       end
     end
     describe "when the object does not have a permanent identifier" do
-      let!(:id) { described_class.new("foo") }
+      let(:obj) { FactoryGirl.create(:item) }
+      let!(:id) { described_class.identifier_class.new("foo") }
       before do
-        allow(described_class).to receive(:find).with("foo") { id }
+        allow(described_class.identifier_class).to receive(:find).with("foo") { id }
         allow(id).to receive(:save) { nil }
-        allow(obj).to receive(:save) { nil }
+        allow(obj).to receive(:save) { true }
       end
       describe "when passed an ARK" do
         before do
-          described_class.assign!(obj, ark: "foo")
+          described_class.assign!(obj, "foo")
         end
         it "assigns the ARK" do
           expect(obj.permanent_id).to eq("foo")
-          expect(id.repo_id).to eq(obj.id)
+        end
+        it "sets the target on the identifier" do
           expect(id.target).to eq("https://repository.duke.edu/id/foo")
+        end
+        it "sets the status on the identifier" do
           expect(id.status).to eq("reserved")
+        end
+        it "sets the repository id on the identifier" do
+          expect(id["fcrepo3.pid"]).to eq(obj.id)
         end
       end
       describe "when not passed an ARK" do
         before do
-          allow(described_class).to receive(:mint) { id }
-          described_class.assign!(obj, ark: "foo")
+          described_class.assign!(obj, "foo")
+          allow(described_class.identifier_class).to receive(:mint) { id }
         end
-        it "mints and ARK and assigns it" do
+        it "mints and assigns an ARK" do
           expect(obj.permanent_id).to eq("foo")
-          expect(id.repo_id).to eq(obj.id)
+        end
+        it "sets the target on the identifier" do
           expect(id.target).to eq("https://repository.duke.edu/id/foo")
+        end
+        it "sets the status on the identifier" do
           expect(id.status).to eq("reserved")
         end
+        it "sets the repository id on the identifier" do
+          expect(id["fcrepo3.pid"]).to eq(obj.id)
+        end
       end
     end
   end
 
-  describe "#repo_id" do
-    subject { described_class.new("foo", "fcrepo3.pid"=>"test:1") }
-    its(:repo_id) { is_expected.to eq("test:1") }
-  end
-
-  describe "#repo_id=" do
-    subject { described_class.new("foo") }
-    specify {
-      subject.repo_id = "test:1"
-      expect(subject["fcrepo3.pid"]).to eq("test:1")
-    }
-    describe "when a value was previously assigned" do
-      before { subject.repo_id = "test:1" }
+  describe "identifier metadata" do
+    let(:obj) { Item.create(pid: "test:1") }
+    let!(:id) { described_class.identifier_class.new("foo") }
+    subject { described_class.new(obj) }
+    before do
+      allow(subject).to receive(:identifier) { id }
+    end
+    describe "#repo_id" do
+      before do
+        allow(id).to receive(:[]).with("fcrepo3.pid") { "test:1" }
+      end
+      its(:repo_id) { is_expected.to eq("test:1") }
+    end
+    describe "#repo_id=" do
       specify {
-        expect { subject.repo_id = "test:2" }.to raise_error(PermanentId::Error)
+        subject.repo_id = "test:1"
+        expect(id["fcrepo3.pid"]).to eq("test:1")
+      }
+      describe "when a value was previously assigned" do
+        before { subject.repo_id = "test:1" }
+        specify {
+          expect { subject.repo_id = "test:2" }.to raise_error(PermanentId::Error)
+        }
+      end
+    end
+    describe "#set_repo_id" do
+      specify {
+        subject.set_repo_id
+        expect(subject.repo_id).to eq("test:1")
       }
     end
-  end
-
-  describe "#set_repo_id" do
-    let(:obj) { Item.new(pid: "test:1") }
-    subject { described_class.new("foo") }
-    specify {
-      subject.set_repo_id(obj)
-      expect(subject.repo_id).to eq("test:1")
-    }
-  end
-
-  describe "#set_target" do
-    subject { described_class.new("foo") }
-    specify {
-      subject.set_target
-      expect(subject.target).to eq("https://repository.duke.edu/id/foo")
-    }
-  end
-
-  describe "#set_status" do
-    subject { described_class.new("foo") }
-    let(:obj) { FactoryGirl.build(:item) }
-    describe "when object is published" do
-      before { obj.workflow_state = "published" }
-      describe "and identifier is public" do
-        before { subject.public! }
-        it "does not change" do
-          expect { subject.set_status(obj) }.not_to change(subject, :status)
-        end
-      end
-      describe "and identifier is reserved" do
-        it "changes to public" do
-          expect { subject.set_status(obj) }.to change(subject, :status).to("public")
-        end
-      end
-      describe "and identifier is unavailable" do
-        before { subject.unavailable! }
-        it "changes to public" do
-          expect { subject.set_status(obj) }.to change(subject, :status).to("public")
-        end
-      end
+    describe "#set_target" do
+      specify {
+        subject.set_target
+        expect(subject.target).to eq("https://repository.duke.edu/id/foo")
+      }
     end
-    describe "when object is unpublished" do
-      before { obj.workflow_state = "unpublished" }
-      describe "and identifier is public" do
-        before { subject.public! }
-        it "changes to unavailable" do
-          expect { subject.set_status(obj) }.to change(subject, :status).to("unavailable | not published")
+    describe "#set_status" do
+      describe "when object is published" do
+        before { obj.workflow_state = "published" }
+        describe "and identifier is public" do
+          before { subject.public! }
+          it "does not change" do
+            expect { subject.set_status }.not_to change(subject, :status)
+          end
+        end
+        describe "and identifier is reserved" do
+          it "changes to public" do
+            expect { subject.set_status }.to change(subject, :status).to("public")
+          end
+        end
+        describe "and identifier is unavailable" do
+          before { subject.unavailable! }
+          it "changes to public" do
+            expect { subject.set_status }.to change(subject, :status).to("public")
+          end
         end
       end
-      describe "and identifier is reserved" do
-        it "does not change" do
-          expect { subject.set_status(obj) }.not_to change(subject, :status)
+      describe "when object is unpublished" do
+        before { obj.workflow_state = "unpublished" }
+        describe "and identifier is public" do
+          before { subject.public! }
+          it "changes to unavailable" do
+            expect { subject.set_status }.to change(subject, :status).to("unavailable | not published")
+          end
+        end
+        describe "and identifier is reserved" do
+          it "does not change" do
+            expect { subject.set_status }.not_to change(subject, :status)
+          end
+        end
+        describe "and identifier is unavailable" do
+          before { subject.unavailable! }
+          it "does not change" do
+            expect { subject.set_status }.not_to change(subject, :status)
+          end
         end
       end
-      describe "and identifier is unavailable" do
-        before { subject.unavailable! }
-        it "does not change" do
-          expect { subject.set_status(obj) }.not_to change(subject, :status)
+      describe "when object has no workflow state" do
+        describe "and identifier is public" do
+          before { subject.public! }
+          it "does not change" do
+            expect { subject.set_status }.not_to change(subject, :status)
+          end
         end
-      end
-    end
-    describe "when object has no workflow state" do
-      describe "and identifier is public" do
-        before { subject.public! }
-        it "does not change" do
-          expect { subject.set_status(obj) }.not_to change(subject, :status)
+        describe "and identifier is reserved" do
+          it "does not change" do
+            expect { subject.set_status }.not_to change(subject, :status)
+          end
         end
-      end
-      describe "and identifier is reserved" do
-        it "does not change" do
-          expect { subject.set_status(obj) }.not_to change(subject, :status)
-        end
-      end
-      describe "and identifier is unavailable" do
-        before { subject.unavailable! }
-        it "does not change" do
-          expect { subject.set_status(obj) }.not_to change(subject, :status)
+        describe "and identifier is unavailable" do
+          before { subject.unavailable! }
+          it "does not change" do
+            expect { subject.set_status }.not_to change(subject, :status)
+          end
         end
       end
     end
   end
-
 end
