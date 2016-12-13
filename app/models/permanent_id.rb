06393f815e305261e375d0411f4aeec528ef087c
David Chandek-Stark <david.chandek.stark@duke.edu>
2016-12-13 10:34:46 -0500

    Completes work of DDR-353 (#1804)
    * WIP [ci skip]
    
    * WIP
    
    * WIP
    
    * Completes work of DDR-353.
    
    * Use constants instead of literals.

--- app/models/permanent_id.rb
+++ app/models/permanent_id.rb
@@ -1,10 +1,13 @@
 require 'ezid-client'
 
-class PermanentId < Ezid::Identifier
+class PermanentId
 
   class Error < DulHydra::Error; end
+  class AssignmentFailed < Error; end
   class ObjectNotPersisted < Error; end
-  class AlreadyAssigned < Error; end
+  class AlreadyAssigned < AssignmentFailed; end
+  class IdentifierNotAssigned < Error; end
+  class IdentifierNotFound < Error; end
 
   DEFAULT_STATUS   = Ezid::Status::RESERVED
   DEFAULT_EXPORT   = "no"
@@ -12,57 +15,98 @@ class PermanentId < Ezid::Identifier
   DEFAULT_TARGET   = "https://repository.duke.edu/id/%s"
   FCREPO3_PID      = "fcrepo3.pid".freeze
 
-  self.defaults = {
-    profile: "dc",
-    export:  "no",
+  class_attribute :identifier_class
+
+  self.identifier_class = Ezid::Identifier
+  self.identifier_class.defaults = {
+    profile: DEFAULT_PROFILE,
+    export:  DEFAULT_EXPORT,
     status:  DEFAULT_STATUS,
   }
 
-  # @return [PermanentId] the permanent id assigned
-  def self.assign!(obj, ark: nil)
-    if obj.new_record?
-      raise ObjectNotPersisted,
-            "Object must be persisted prior to permanent id assignment."
-    end
-    if obj.permanent_id
-      raise AlreadyAssigned,
-            "Object #{obj.id} has already been assigned permanent id \"#{obj.permanent_id}\"."
-    end
-    identifier = ark ? find(ark) : mint
-    obj.reload
-    obj.permanent_id = identifier.id
-    obj.save(validate: false)
-    identifier.set_metadata!(obj)
+  def self.update!(object_or_id)
+    perm_id = new(object_or_id)
+    perm_id.update! if perm_id.assigned?
   end
 
-  # @return
-  def self.assigned(obj)
-    find(obj.permanent_id) if obj.permanent_id
+  # @return [PermanentId] the permanent id assigned to the object
+  def self.assign!(object_or_id, ark = nil)
+    new(object_or_id).assign!(ark)
+  end
+
+  # @return [PermanentId] the permanent id previously assigned to the object,
+  #   or nil, if not assigned.
+  def self.assigned(object_or_id)
+    perm_id = new(object_or_id)
+    perm_id.assigned? ? perm_id : nil
+  end
+
+  attr_reader :object
+
+  def initialize(object_or_id)
+    object = case object_or_id
+             when ActiveFedora::Base
+               if object_or_id.new_record?
+                 raise ObjectNotPersisted, "Object must be persisted."
+               end
+               object_or_id
+             when String
+               ActiveFedora::Base.find(object_or_id)
+             else
+               raise TypeError, "#{object_or_id.class} is not expected."
+             end
+    @object = object
+  end
+
+  def assign!(id = nil)
+    ActiveSupport::Notifications.instrument("assign.permanent_id", pid: object.id) do |payload|
+      assign(id)
+      payload.merge(status: status, target: target, permanent_id: identifier.id)
+    end
+  end
+
+  def assigned?
+    object.permanent_id
+  end
+
+  def update!
+    ActiveSupport::Notifications.instrument("update.permanent_id", pid: object.id) do |payload|
+      update
+      payload.merge(status: status, permanent_id: identifier.id)
+    end
+  end
+
+  def identifier
+    if @identifier.nil? && assigned?
+      @identifier = find_identifier(object.permanent_id)
+    end
+    @identifier
   end
 
   # @raise [Ezid::Error] on EZID client or server error
-  def set_metadata!(obj)
-    set_metadata(obj)
+  def set_metadata!
+    set_metadata
     save
+    self
   end
 
-  def set_metadata(obj)
+  def set_metadata
     set_target
-    set_status(obj)
-    set_repo_id(obj)
+    set_status
+    set_repo_id
   end
 
   def set_target
     self.target = DEFAULT_TARGET % id
   end
 
-  def set_repo_id(obj)
-    self.repo_id = obj.id
+  def set_repo_id
+    self.repo_id = object.id
   end
 
   def repo_id=(val)
     if repo_id
-      raise Error, "#{repo_id_field} already set to \"#{repo_id}\", cannot change."
+      raise Error, "Identifier repository id already set to \"#{repo_id}\"; cannot change."
     end
     self[repo_id_field] = val
   end
@@ -75,12 +119,61 @@ class PermanentId < Ezid::Identifier
     FCREPO3_PID
   end
 
-  def set_status(obj)
-    if obj.published? && !public?
+  def set_status!
+    save if set_status
+  end
+
+  def set_status
+    if object.published? && !public?
       public!
-    elsif obj.unpublished? && public?
+    elsif object.unpublished? && public?
       unavailable!("not published")
     end
+  end
+
+  protected
+
+  def method_missing(name, *args, &block)
+    identifier.send(name, *args, &block)
+  end
+
+  private
+
+  def find_identifier(ark)
+    identifier_class.find(ark)
+  rescue Ezid::IdentifierNotFoundError => e
+    raise IdentifierNotFound, e.message
+  end
+
+  def mint_identifier(*args)
+    identifier_class.mint(*args)
+  end
+
+  def update
+    if !assigned?
+      raise IdentifierNotAssigned,
+            "Cannot update identifier for object \"#{object.id}\"; not assigned."
+    end
+    set_status!
+  end
+
+  def assign(id = nil)
+    if assigned?
+      raise AlreadyAssigned,
+            "Object \"#{object.id}\" has already been assigned permanent id \"#{object.permanent_id}\"."
+    end
+    @identifier = case id
+                  when identifier_class
+                    id
+                  when String
+                    find_identifier(id)
+                  when nil
+                    mint_identifier
+                  end
+    object.reload
+    object.permanent_id = identifier.id
+    object.save(validate: false)
+    set_metadata!
   end
 
 end
