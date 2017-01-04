509f948ccf0ce07ee9d7aea8edc1bb0fcd59b908
David Chandek-Stark <david.chandek.stark@duke.edu>
2017-01-04 16:48:46 -0500

    Upgrades ddr-models to v2.6.0.rc2 (#1810)
    Moves PermanentId class to ddr-models.

--- spec/lib/dul_hydra_spec.rb
+++ spec/lib/dul_hydra_spec.rb
@@ -2,8 +2,6 @@ RSpec.describe DulHydra do
 
   describe "configuration defaults" do
     subject { described_class }
-    its(:auto_assign_permanent_id) { is_expected.to be false }
-    its(:auto_update_permanent_id) { is_expected.to be false }
     its(:fixity_check_limit) { is_expected.to eq(10000) }
     its(:fixity_check_period_in_days) { is_expected.to eq(60) }
     its(:batches_per_page) { is_expected.to eq(10) }
