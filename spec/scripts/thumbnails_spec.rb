0aa0ddc0b669e373ce9fa0f3b77d46f9a1b313d8
Jim Coble <jim.coble@duke.edu>
2017-02-23 13:27:56 -0500

    Fixes error being thrown by thumbnails script spec in certain circumstances; fixes DDR-573.

--- spec/scripts/thumbnails_spec.rb
+++ spec/scripts/thumbnails_spec.rb
@@ -4,50 +4,54 @@ module DulHydra::Scripts
 
   describe Thumbnails do
 
-    let(:item) { FactoryGirl.create(:item, :member_of_collection, :has_part) }
+    let(:item) { FactoryGirl.create(:item, :member_of_collection) }
+    let(:component) { Component.create }
     let(:collection) { item.collection }
-    let(:component) { item.children.first }
     let(:thumbnails_script) { DulHydra::Scripts::Thumbnails.new(collection.pid) }
+    let(:thumbnail) { fixture_file_upload('target.png') }
+    let(:thumbnail_checksum) { Ddr::Utils.digest(File.read(thumbnail), 'SHA-1') }
+
+    before do
+      item.children << component
+      item.save!
+    end
 
     context "thumbnail does not exist" do
 
       context "child has thumbnail" do
         before do
-          thumbnails_script.execute
-          item.reload
+          component.thumbnail.content = thumbnail
+          component.save!
         end
         it "should populate the thumbnail datastream from the child thumbnail" do
-          expect(item.datastreams['thumbnail'].checksum).to eq(component.datastreams['thumbnail'].checksum)
+          thumbnails_script.execute
+          item.reload
+          expect(item.thumbnail.checksum).to eq(thumbnail_checksum)
         end
       end
 
       context "child does not have thumbnail" do
-        before do
-          component.datastreams['content'].delete
-          component.save!
-          component.reload
-          component.datastreams['thumbnail'].delete
-          component.save!
+        it "should not populate the thumbnail datastream" do
           thumbnails_script.execute
           item.reload
-        end
-        it "should not populate the thumbnail datastream" do
-          expect(item.datastreams["thumbnail"]).to_not have_content
+          expect(item.thumbnail).to_not have_content
         end
       end
 
     end
 
     context "thumbnail already exists" do
-      let(:content) { StringIO.new("awesome image") }
+      let(:item_thumbnail) { StringIO.new("awesome image") }
       before do
-        item.datastreams["thumbnail"].content = content
+        component.thumbnail.content = thumbnail
+        component.save!
+        item.thumbnail.content = item_thumbnail
         item.save!
-        thumbnails_script.execute
-        item.reload
       end
       it "should not alter the existing thumbnail" do
-        expect(item.datastreams["thumbnail"].content).to eq("awesome image")
+        thumbnails_script.execute
+        item.reload
+        expect(item.thumbnail.content).to eq("awesome image")
       end
     end
 
