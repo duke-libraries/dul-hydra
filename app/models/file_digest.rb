fb0814ad102d359b151f72b3a0f5230126333aec
David Chandek-Stark <dchandekstark@gmail.com>
2017-06-06 10:25:22 -0400

    Revert "Adds md5 and path columns to file_digests. (#1878)"
    This reverts commit e36c6365eb18f7b0ddf12e9675d90d97489195b0.
    
    Note that the database migration was not removed; a new migration
    removing the added columns was added; so, this is not a true
    revert of the commit.

--- app/models/file_digest.rb
+++ app/models/file_digest.rb
@@ -2,21 +2,22 @@ require 'digest'
 
 class FileDigest < ActiveRecord::Base
 
-  validates_presence_of :repo_id, :file_id, :path
+  validates_presence_of :repo_id, :file_id, :sha1
+
+  def self.sha1(repo_id, file_id)
+    if fd = find_by(repo_id: repo_id, file_id: file_id)
+      fd.sha1
+    end
+  end
 
   def self.generate_sha1(path)
     Digest::SHA1.file(path).hexdigest
   end
 
-  def self.generate_md5(path)
-    Digest::MD5.file(path).hexdigest
-  end
+  delegate :generate_sha1, to: :class
 
-  delegate :generate_sha1, :generate_md5, to: :class
-
-  def set_digests
+  def set_digest(path)
     self.sha1 = generate_sha1(path)
-    self.md5  = generate_md5(path)
   end
 
 end
--- spec/models/file_digest_spec.rb
+++ spec/models/file_digest_spec.rb
@@ -1,42 +1,40 @@
 RSpec.describe FileDigest do
 
-  subject { described_class.new(path: file.path, repo_id: "test:1", file_id: "content") }
-
   let(:file) { fixture_file_upload('sample.pdf') }
-
   let(:sha1) { 'a6ae0d815c1a2aef551b45fe34a35ceea1828a4d' }
-  let(:md5) { '4f3f9c99a9f2720b77870371ff21ea9f' }
 
-  describe ".generate_sha1" do
+  describe "sha1" do
+    let(:repo_id) { 'test:1' }
+    let(:file_id) { 'content' }
+    describe "record exists" do
+      before do
+        described_class.create(repo_id: 'test:1', file_id: 'content', sha1: sha1)
+      end
+      specify {
+        expect(described_class.sha1(repo_id, file_id)).to eq sha1
+      }
+    end
+    describe "record does not exist" do
+      specify {
+        expect(described_class.sha1(repo_id, file_id)).to be nil
+      }
+    end
+  end
+
+  describe "generate_sha1" do
     specify {
       expect(described_class.generate_sha1(file.path)).to eq sha1
     }
-  end
-
-  describe "#generate_sha1" do
     specify {
       expect(subject.generate_sha1(file.path)).to eq sha1
     }
   end
 
-  describe ".generate_md5" do
-    specify {
-      expect(described_class.generate_md5(file.path)).to eq md5
-    }
-  end
-
-  describe "#generate_md5" do
-    specify {
-      expect(subject.generate_md5(file.path)).to eq md5
-    }
-  end
-
-  describe "set_digests" do
+  describe "set_digest" do
     before do
-      subject.set_digests
+      subject.set_digest(file.path)
     end
     its(:sha1) { is_expected.to eq sha1 }
-    its(:md5) { is_expected.to eq md5 }
   end
 
 end
