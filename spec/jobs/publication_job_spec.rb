4d157b57fe952e14f7e03fa2c5e6e70ca031931a
Jim Coble <jim.coble@duke.edu>
2018-04-03 13:08:04 -0400

    Fix bug where publication jobs were not sending emails at completion or failure; DDR-1447.

--- spec/jobs/publication_job_spec.rb
+++ spec/jobs/publication_job_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 
 RSpec.describe PublicationJob, type: :job do
 
+  describe '.send_notification' do
+    let(:email) { 'nobody@example.com' }
+    let(:subject) { 'Test Email' }
+    let(:message) { 'Text of test message' }
+    it 'sends an email' do
+      expect{ described_class.send_notification(email: email, subject: subject, message: message) }
+        .to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+
   describe '.publication_scope' do
     it 'returns the correct string' do
       expect(described_class.publication_scope(Collection.new)).to eq(I18n.t('dul_hydra.publication.scope.collection'))
